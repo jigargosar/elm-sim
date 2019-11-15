f508823212719b647c84b20819face9a0522e88f
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-15 16:09:34 +0530

    init

--- src/Main.elm
+++ src/Main.elm
@@ -8,8 +8,42 @@ type alias Screen =
     { w : Float, h : Float }
 
 
+type alias Flags =
+    {}
+
+
+type alias Model =
+    {}
+
+
+init : Flags -> ( Model, Cmd msg )
+init flags =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+subscriptions : Model -> Sub msg
+subscriptions _ =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
 main =
     Browser.element
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
 
 
 view _ =

4a602bee2df79ffd86cc2b3b8fd5e8f6a332ddb6
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-15 19:12:44 +0530

    init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (_, config) => {
+  const isProd = config.mode === 'production'
+  return {
+    output: {
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.js', '.elm'],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+    module: {
+      rules: [
+        {
+          include: /\.elm/,
+          use: [
+            'elm-hot-webpack-loader',
+            { loader: 'elm-webpack-loader', options: { optimize: isProd , debug: false} },
+          ],
+        },
+        { include: /\.css/, use: ['style-loader', 'css-loader'] },
+      ],
+    },
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      overlay: true,
+    },
+  }
+}

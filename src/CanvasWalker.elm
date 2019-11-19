a288499c22e0eb29add7a0303db590155264a649
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-19 16:13:54 +0530

    Game of Life

--- src/CanvasWalker.elm
+++ src/CanvasWalker.elm
@@ -5,6 +5,7 @@ import Browser
 import Browser.Dom exposing (Viewport, getViewport)
 import Browser.Events exposing (onAnimationFrameDelta, onResize)
 import Class
+import GridOfLife as GOL
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Random exposing (Generator, Seed)
@@ -19,8 +20,8 @@ type alias Flags =
 
 type alias Model =
     { collectedDelta : Float
-    , grid : Grid
-    , lastGridStates : List Grid
+    , grid : GOL.Grid
+    , lastGridStates : List GOL.Grid
     , width : Float
     , height : Float
     , seed : Seed
@@ -32,7 +33,7 @@ init flags =
     ( { collectedDelta = 0
       , width = 400
       , height = 400
-      , grid = emptyGrid
+      , grid = GOL.emptyGrid gridConfig
       , lastGridStates = []
       , seed = Random.initialSeed flags.now
       }
@@ -41,130 +42,11 @@ init flags =
     )
 
 
-type Cell
-    = On
-    | Off
-
-
-type alias GridRow =
-    Array Cell
-
-
-type alias Grid =
-    Array GridRow
-
-
-cellAtRC : Int -> Int -> Grid -> Cell
-cellAtRC row col grid =
-    Array.get row grid
-        |> Maybe.andThen (Array.get col)
-        |> Maybe.withDefault Off
-
-
-neighbours : List ( Int, Int )
-neighbours =
-    [ ( -1, -1 ), ( -1, 0 ), ( -1, 1 ) ]
-        ++ [ ( 0, -1 ), {- ignore self (0,0) , -} ( 0, 1 ) ]
-        ++ [ ( 1, -1 ), ( 1, 0 ), ( 1, 1 ) ]
-
-
-neighboursOfRC : Int -> Int -> Grid -> List Cell
-neighboursOfRC row col grid =
-    neighbours
-        |> List.map (\( nr, nc ) -> cellAtRC (row + nr) (col + nc) grid)
-
-
-is =
-    (==)
-
-
-aliveNeighbourCountOfCellAtRC : Int -> Int -> Grid -> Int
-aliveNeighbourCountOfCellAtRC row col grid =
-    neighboursOfRC row col grid
-        |> List.filter (is On)
-        |> List.length
-
-
-{-| <https://www.conwaylife.com/wiki/Conway's_Game_of_Life>
--}
-nextStateOfCell : Int -> Cell -> Cell
-nextStateOfCell aliveNeighbourCount cell =
-    case cell of
-        On ->
-            {- Any live cell with fewer than two live neighbours dies
-                (referred to as underpopulation or exposure[1]).
-               Any live cell with more than three live neighbours dies
-                (referred to as overpopulation or overcrowding).
-            -}
-            if aliveNeighbourCount < 2 || aliveNeighbourCount > 3 then
-                Off
-
-            else
-                {- Any live cell with two or three live neighbours lives,
-                   unchanged, to the next generation.
-                -}
-                On
-
-        Off ->
-            if aliveNeighbourCount == 3 then
-                On
-
-            else
-                Off
-
-
-nextStateOfCellAtRC : Int -> Int -> Grid -> Cell
-nextStateOfCellAtRC row col grid =
-    let
-        aliveNeighbourCount =
-            aliveNeighbourCountOfCellAtRC row col grid
-
-        cell =
-            cellAtRC row col grid
-    in
-    nextStateOfCell aliveNeighbourCount cell
-
-
-mapGridRC : (Int -> Int -> a -> b) -> Array (Array a) -> Array (Array b)
-mapGridRC func =
-    Array.indexedMap (\rowNum -> Array.indexedMap (\colNum -> func rowNum colNum))
-
-
-nextGridState : Grid -> Grid
-nextGridState grid =
-    mapGridRC (\r c _ -> nextStateOfCellAtRC r c grid) grid
-
-
-emptyGrid : Grid
-emptyGrid =
-    Array.repeat gridConfig.colCount Off
-        |> Array.repeat gridConfig.rowCount
-
-
-randomArray : Int -> Generator a -> Generator (Array a)
-randomArray count =
-    Random.list count >> Random.map Array.fromList
-
-
-randomGrid : Generator Grid
-randomGrid =
-    let
-        randomGridCell : Generator Cell
-        randomGridCell =
-            Random.weighted ( 80, Off ) [ ( 20, On ) ]
-
-        randomGridRow : Generator (Array Cell)
-        randomGridRow =
-            randomArray gridConfig.colCount randomGridCell
-    in
-    randomArray gridConfig.rowCount randomGridRow
-
-
 randomizeGrid : Model -> Model
 randomizeGrid model =
     let
         ( grid, seed ) =
-            Random.step randomGrid model.seed
+            Random.step (GOL.randomGrid gridConfig) model.seed
     in
     { model | grid = grid, seed = seed }
 
@@ -235,11 +117,11 @@ step model =
 
 updateOnFrame : Model -> Model
 updateOnFrame model =
-    { model | grid = nextGridState model.grid }
+    { model | grid = GOL.nextGridState model.grid }
         |> pushLastGridState model.grid
 
 
-pushLastGridState : Grid -> Model -> Model
+pushLastGridState : GOL.Grid -> Model -> Model
 pushLastGridState lastGrid model =
     { model | lastGridStates = lastGrid :: model.lastGridStates |> List.take 2 }
 
@@ -263,25 +145,25 @@ gridConfig =
     }
 
 
-viewGrid : GridConfig -> Grid -> Html msg
+viewGrid : GridConfig -> GOL.Grid -> Html msg
 viewGrid config grid =
     let
         viewGridCell : Int -> Int -> Html msg
         viewGridCell rowNum colNum =
             let
-                cell : Cell
+                cell : GOL.Cell
                 cell =
-                    cellAtRC rowNum colNum grid
+                    GOL.cellAtRC rowNum colNum grid
             in
             div
                 [ Style.widthPx config.cellSize
                 , Style.heightPx config.cellSize
                 , Style.bgColor
                     (case cell of
-                        Off ->
+                        GOL.Off ->
                             "yellow"
 
-                        On ->
+                        GOL.On ->
                             "red"
                     )
                 , Style.noShrink
---
+++ src/GridOfLife.elm
@@ -0,0 +1,121 @@
+module GridOfLife exposing (..)
+
+import Array exposing (Array)
+import Random exposing (Generator)
+
+
+type Cell
+    = On
+    | Off
+
+
+type alias GridRow =
+    Array Cell
+
+
+type alias Grid =
+    Array GridRow
+
+
+cellAtRC : Int -> Int -> Grid -> Cell
+cellAtRC row col grid =
+    Array.get row grid
+        |> Maybe.andThen (Array.get col)
+        |> Maybe.withDefault Off
+
+
+neighbours : List ( Int, Int )
+neighbours =
+    [ ( -1, -1 ), ( -1, 0 ), ( -1, 1 ) ]
+        ++ [ ( 0, -1 ), {- ignore self (0,0) , -} ( 0, 1 ) ]
+        ++ [ ( 1, -1 ), ( 1, 0 ), ( 1, 1 ) ]
+
+
+neighboursOfRC : Int -> Int -> Grid -> List Cell
+neighboursOfRC row col grid =
+    neighbours
+        |> List.map (\( nr, nc ) -> cellAtRC (row + nr) (col + nc) grid)
+
+
+is =
+    (==)
+
+
+aliveNeighbourCountOfCellAtRC : Int -> Int -> Grid -> Int
+aliveNeighbourCountOfCellAtRC row col grid =
+    neighboursOfRC row col grid
+        |> List.filter (is On)
+        |> List.length
+
+
+{-| <https://www.conwaylife.com/wiki/Conway's_Game_of_Life>
+-}
+nextStateOfCell : Int -> Cell -> Cell
+nextStateOfCell aliveNeighbourCount cell =
+    case cell of
+        On ->
+            {- Any live cell with fewer than two live neighbours dies
+                (referred to as underpopulation or exposure[1]).
+               Any live cell with more than three live neighbours dies
+                (referred to as overpopulation or overcrowding).
+            -}
+            if aliveNeighbourCount < 2 || aliveNeighbourCount > 3 then
+                Off
+
+            else
+                {- Any live cell with two or three live neighbours lives,
+                   unchanged, to the next generation.
+                -}
+                On
+
+        Off ->
+            if aliveNeighbourCount == 3 then
+                On
+
+            else
+                Off
+
+
+nextStateOfCellAtRC : Int -> Int -> Grid -> Cell
+nextStateOfCellAtRC row col grid =
+    let
+        aliveNeighbourCount =
+            aliveNeighbourCountOfCellAtRC row col grid
+
+        cell =
+            cellAtRC row col grid
+    in
+    nextStateOfCell aliveNeighbourCount cell
+
+
+mapGridRC : (Int -> Int -> a -> b) -> Array (Array a) -> Array (Array b)
+mapGridRC func =
+    Array.indexedMap (\rowNum -> Array.indexedMap (\colNum -> func rowNum colNum))
+
+
+nextGridState : Grid -> Grid
+nextGridState grid =
+    mapGridRC (\r c _ -> nextStateOfCellAtRC r c grid) grid
+
+
+emptyGrid gridConfig =
+    Array.repeat gridConfig.colCount Off
+        |> Array.repeat gridConfig.rowCount
+
+
+randomArray : Int -> Generator a -> Generator (Array a)
+randomArray count =
+    Random.list count >> Random.map Array.fromList
+
+
+randomGrid gridConfig =
+    let
+        randomGridCell : Generator Cell
+        randomGridCell =
+            Random.weighted ( 80, Off ) [ ( 20, On ) ]
+
+        randomGridRow : Generator (Array Cell)
+        randomGridRow =
+            randomArray gridConfig.colCount randomGridCell
+    in
+    randomArray gridConfig.rowCount randomGridRow

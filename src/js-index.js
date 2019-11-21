8c90ab884b9dea6f9ee81cd3b38bd453a37db7c8
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-21 15:29:46 +0530

    Game of Life js

--- src/js-index.js
+++ src/js-index.js
@@ -6,13 +6,14 @@ const ctx = canvasEl.getContext('2d')
 
 const gridConfig = { rowCount: 30, colCount: 30 }
 const canvasSize = { width: ctx.canvas.width, height: ctx.canvas.height }
-const cellSize = (canvasSize.width - 2) / gridConfig.colCount
+const cellSize = (canvasSize.width - 4) / gridConfig.colCount
 
 function drawCell(x, y) {
   ctx.beginPath()
   ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'
   ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)'
-  ctx.rect(x * cellSize, y * cellSize, cellSize, cellSize)
+  ctx.lineWidth = 1
+  ctx.rect(x * cellSize + 1, y * cellSize + 1, cellSize, cellSize)
   ctx.fill()
   ctx.stroke()
 }

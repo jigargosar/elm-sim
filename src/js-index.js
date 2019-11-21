3d1014d17f195d151ddee366a8e6b9bba3251dc5
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-21 15:24:45 +0530

    Game of Life js

--- src/js-index.js
+++ src/js-index.js
@@ -1,14 +1,31 @@
+import { modulo } from 'ramda'
+
 const canvasEl = document.getElementById('canvas-el')
 
 const ctx = canvasEl.getContext('2d')
 
+const gridConfig = { rowCount: 30, colCount: 30 }
+const canvasSize = { width: ctx.canvas.width, height: ctx.canvas.height }
+const cellSize = (canvasSize.width - 2) / gridConfig.colCount
 
-ctx.fillStyle = 'rgb(200, 0, 0)';
-ctx.fillRect(10, 10, 50, 50);
+function drawCell(x, y) {
+  ctx.beginPath()
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'
+  ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)'
+  ctx.rect(x * cellSize, y * cellSize, cellSize, cellSize)
+  ctx.fill()
+  ctx.stroke()
+}
 
-// ctx.fillRect(30, 30, 50, 50);
-ctx.beginPath()
-ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-ctx.rect(30, 30, 50, 50);
-ctx.fill()
-ctx.stroke()
+const cellCount = gridConfig.rowCount * gridConfig.colCount
+
+const grid = new Array(cellCount).fill(0)
+
+grid.forEach((cell, i) => {
+  const [x, y] = [
+    Math.round(i % gridConfig.colCount),
+    Math.floor(i / gridConfig.rowCount),
+  ]
+  console.log(x, y)
+  drawCell(x, y)
+})

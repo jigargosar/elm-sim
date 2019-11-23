25db3ed509a3617337da5ba0d1bbd0c8c8c990f0
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-23 19:41:28 +0530

    modeling GOL Grid

--- src/GOLGrid.elm
+++ src/GOLGrid.elm
@@ -2,6 +2,7 @@ module GOLGrid exposing (Grid, initDead, randomize)
 
 import Dict exposing (Dict)
 import Random exposing (Generator)
+import Random.Extra
 
 
 type alias Pos =
@@ -36,12 +37,20 @@ type alias Data =
 
 
 type alias Grid =
-    { w : Int, h : Int, data : Data }
+    { w : Int
+    , h : Int
+    , data : Data
+    , cords : List Pos
+    }
 
 
 initDead : Int -> Int -> Grid
 initDead w h =
-    { w = w, h = h, data = Dict.empty }
+    { w = w
+    , h = h
+    , data = Dict.empty
+    , cords = toCords w h
+    }
 
 
 randomize : Grid -> Generator Grid
@@ -49,15 +58,43 @@ randomize grid =
     randomDataGenerator grid |> Random.map (\data -> { grid | data = data })
 
 
-randomDataGenerator : HasWH xx -> Generator Data
-randomDataGenerator { w, h } =
+toCords : Int -> Int -> List Pos
+toCords w h =
     let
+        widthRange =
+            List.range 0 (w - 1)
+
+        heightRange =
+            List.range 0 (h - 1)
+    in
+    List.concatMap (\y -> List.map (\x -> ( x, y )) widthRange) heightRange
+
+
+type alias HasGridConfig xx =
+    { xx | w : Int, h : Int, l : Int, cords : List Pos }
+
+
+randomDataGenerator : HasGridConfig xx -> Generator Data
+randomDataGenerator gc =
+    let
+        { l, cords } =
+            gc
+
         maybePosCellGen : Pos -> Generator (Maybe ( Pos, Cell ))
         maybePosCellGen pos =
             Random.weighted ( 20, Just Alive ) [ ( 80, Nothing ) ]
                 |> Random.map (Maybe.map (Tuple.pair pos))
 
-        _ =
-            Random.list (w * h)
+        posCellListGenerator : Generator (List ( Pos, Cell ))
+        posCellListGenerator =
+            cords
+                |> List.map maybePosCellGen
+                |> Random.Extra.combine
+                |> Random.map (List.filterMap identity)
     in
-    Random.constant Dict.empty
+    posCellListGenerator |> Random.map (dataGeneratorFromPosCellList gc)
+
+
+dataGeneratorFromPosCellList : HasGridConfig xx -> List ( Pos, Cell ) -> Data
+dataGeneratorFromPosCellList gc =
+    always Dict.empty

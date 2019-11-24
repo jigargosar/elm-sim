f68f6111aecae54cf9d03640cda8e3b287e75474
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-24 17:08:05 +0530

    extracting common view code

---
+++ src/View.elm
@@ -0,0 +1,34 @@
+module View exposing (cell)
+
+import Color
+import Svg exposing (Svg)
+import TypedSvg exposing (rect)
+import TypedSvg.Attributes exposing (fill)
+import TypedSvg.Attributes.InPx exposing (height, width, x, y)
+import TypedSvg.Types exposing (Fill, px)
+
+
+cell : Float -> Int -> Int -> Bool -> Svg msg
+cell cellWidthInPx gridX gridY isAlive =
+    let
+        xInPx =
+            toFloat gridX * cellWidthInPx + 1
+
+        yInPx =
+            toFloat gridY * cellWidthInPx + 1
+    in
+    rect
+        [ (if isAlive then
+            Color.lightRed
+
+           else
+            Color.lightYellow
+          )
+            |> Fill
+            |> fill
+        , x xInPx
+        , y yInPx
+        , width cellWidthInPx
+        , height cellWidthInPx
+        ]
+        []

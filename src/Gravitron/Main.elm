69a1fb2170208382cd8bc52eb4608040b527d454
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-26 15:06:49 +0530

    Gravitron

--- src/Gravitron/Main.elm
+++ src/Gravitron/Main.elm
@@ -1,7 +1,64 @@
 module Gravitron.Main exposing (..)
 
-import Html exposing (text)
+import Browser
+import Browser.Events
+import Html exposing (Html, text)
+import Random exposing (Seed)
+
+
+
+-- Model
+
+
+type alias Flags =
+    { now : Int }
+
+
+type alias Model =
+    { seed : Seed
+    }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { seed = Random.initialSeed flags.now }
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = Tick Float
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Browser.Events.onAnimationFrameDelta Tick
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        Tick _ ->
+            ( model, Cmd.none )
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view _ =
+    text "hi"
 
 
 main =
-    text "hi"
+    Browser.element
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }

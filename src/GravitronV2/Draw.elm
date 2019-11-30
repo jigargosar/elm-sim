303414782bd28fd6eb00805f18a4782b5ae13584
Jigar Gosar <jigar.gosar@gmail.com>
2019-11-30 17:27:54 +0530

    Gravitron: using elm-units and elm-geom

--- src/GravitronV2/Draw.elm
+++ src/GravitronV2/Draw.elm
@@ -1,13 +1,150 @@
-module GravitronV2.Draw exposing (Color, Screen, black, canvas, fillRectLeftTop, fullScreenCanvas, screenFromWidthHeight)
+module GravitronV2.Draw exposing (Color, Screen, black, fullScreenCanvas, main, screenFromWidthHeight)
 
+import Browser
+import Browser.Dom
+import Browser.Events
 import Color
 import Html exposing (Html)
 import Html.Attributes
+import Json.Decode as JD
 import Svg exposing (Svg)
+import Task
 import TypedSvg
 import TypedSvg.Attributes
 import TypedSvg.Attributes.InPx as InPx
 import TypedSvg.Types
+
+
+
+-- Model
+
+
+type alias Flags =
+    ()
+
+
+type alias Model =
+    { mouse : Mouse
+    , screen : Screen
+    }
+
+
+type alias Mouse =
+    { x : Float
+    , y : Float
+    }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( { mouse = Mouse 100 100
+      , screen = screenFromWidthHeight 600 600
+      }
+    , Browser.Dom.getViewport |> Task.perform OnViewport
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = Tick Float
+    | MouseMoved Float Float
+    | OnResize Int Int
+    | OnViewport Browser.Dom.Viewport
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    [ Browser.Events.onAnimationFrameDelta Tick
+    , JD.map2 MouseMoved
+        (JD.field "pageX" JD.float)
+        (JD.field "pageY" JD.float)
+        |> Browser.Events.onMouseMove
+    , Browser.Events.onResize OnResize
+    ]
+        |> Sub.batch
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        Tick _ ->
+            ( onTick model
+            , Cmd.none
+            )
+
+        MouseMoved mx my ->
+            let
+                mouse =
+                    model.mouse
+
+                screen =
+                    model.screen
+            in
+            ( { model | mouse = { mouse | x = mx + screen.left, y = my + screen.top } }
+            , Cmd.none
+            )
+
+        OnViewport { scene } ->
+            ( { model | screen = screenFromWidthHeight scene.width scene.height }
+            , Cmd.none
+            )
+
+        OnResize width height ->
+            ( { model | screen = screenFromWidthHeight (toFloat width) (toFloat height) }
+            , Cmd.none
+            )
+
+
+onTick : Model -> Model
+onTick model =
+    model
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    let
+        screen =
+            model.screen
+
+        x =
+            screen.left
+
+        y =
+            screen.top
+
+        w =
+            screen.width
+
+        h =
+            screen.height
+    in
+    fullScreenCanvas x
+        y
+        w
+        h
+        black
+        []
+
+
+
+-- Program
+
+
+main : Program Flags Model Msg
+main =
+    Browser.element
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
 
 
 type alias Screen =

5f170bd632aad5459c4555ebe2289bf6a5ccb3d4
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-10 12:48:43 +0530

    GravitronV3: re modeling

--- src/GravitronV3/Screen.elm
+++ src/GravitronV3/Screen.elm
@@ -1,6 +1,11 @@
 module GravitronV3.Screen exposing (Screen, fromViewport, initial)
 
 import Browser.Dom as Dom
+import Html exposing (Html)
+import Html.Attributes as H
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import TypedSvg.Attributes as T
 
 
 type alias Number =
@@ -36,3 +41,17 @@ toScreen ( width, height ) =
 initial : Screen
 initial =
     toScreen ( 600, 600 )
+
+
+toSvg : Screen -> List (Svg msg) -> Html msg
+toSvg screen =
+    svg
+        [ T.viewBox 0 0 300 300
+        , width "100%"
+        , height "100%"
+        , H.style "position" "fixed"
+        , H.style "top" "0"
+        , H.style "left" "0"
+        , width "100%"
+        , height "100%"
+        ]

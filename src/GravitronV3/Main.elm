bf86dbeec6fadcfb0c082803af163a9ddb51c78c
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-10 16:38:21 +0530

    GravitronV3: re modeling

--- src/GravitronV3/Main.elm
+++ src/GravitronV3/Main.elm
@@ -29,8 +29,7 @@ view { screen } =
 
 renderShapes : Screen -> List Shape -> Html msg
 renderShapes screen shapes =
-    Screen.toSvg screen
-        (List.map renderShape shapes)
+    Screen.toSvg screen (List.map renderShape shapes)
 
 
 type alias Model =

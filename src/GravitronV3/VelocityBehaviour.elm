cc97f283058726b55e56369e63df126ecfa07b20
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-12 18:34:41 +0530

    GravitronV3-2: re modeling

---
+++ src/GravitronV3/VelocityBehaviour.elm
@@ -0,0 +1,93 @@
+module GravitronV3.VelocityBehaviour exposing
+    ( VelocityBehaviour
+    , initWanderAndBounceInScreen
+    , update
+    )
+
+import GravitronV3.Screen exposing (Screen)
+import GravitronV3.Vec as Vec exposing (Vec, vec)
+import List.Extra
+import Random exposing (Generator, Seed)
+import Random.Float
+
+
+type VelocityBehaviour
+    = Wander Seed
+    | BounceInScreen Float
+    | Batch (List VelocityBehaviour)
+
+
+initWanderAndBounceInScreen : Float -> VelocityBehaviour
+initWanderAndBounceInScreen factor =
+    Batch [ Wander (Random.initialSeed 0), BounceInScreen factor ]
+
+
+randomWalkerVelocity : Vec -> Generator Vec
+randomWalkerVelocity velocity =
+    let
+        randomAngle : Generator Float
+        randomAngle =
+            Random.Float.standardNormal |> Random.map ((*) 0.005 >> turns)
+    in
+    randomAngle
+        |> Random.map
+            (\newAngleDiff ->
+                velocity
+                    |> Vec.mapAngle ((+) newAngleDiff)
+                    |> Vec.mapMagnitude (max 0.01)
+            )
+
+
+update : Screen -> { a | position : Vec, velocity : Vec } -> VelocityBehaviour -> ( Vec, VelocityBehaviour )
+update screen { position, velocity } model =
+    update2 screen position velocity model
+
+
+update2 : Screen -> Vec -> Vec -> VelocityBehaviour -> ( Vec, VelocityBehaviour )
+update2 screen position velocity model =
+    case model of
+        Wander seed ->
+            Random.step (randomWalkerVelocity velocity) seed
+                |> Tuple.mapSecond Wander
+
+        BounceInScreen bounceFactor ->
+            ( bounceWithinScreen screen
+                position
+                bounceFactor
+                velocity
+            , model
+            )
+
+        Batch list ->
+            List.Extra.mapAccuml (update2 screen position) velocity list
+                |> Tuple.mapSecond Batch
+
+
+bounceWithinScreen : Screen -> Vec -> Float -> Vec -> Vec
+bounceWithinScreen screen position bounceFactor velocity =
+    let
+        bounceVelocityPart lo high positionPart velocityPart =
+            if
+                (positionPart < lo && velocityPart < 0)
+                    || (positionPart > high && velocityPart > 0)
+            then
+                negate velocityPart
+
+            else
+                velocityPart
+
+        ( x, y ) =
+            Vec.toTuple position
+
+        ( vx, vy ) =
+            Vec.toTuple velocity
+
+        newBouncedVelocity =
+            vec (bounceVelocityPart screen.left screen.right x vx)
+                (bounceVelocityPart screen.top screen.bottom y vy)
+    in
+    if velocity /= newBouncedVelocity then
+        newBouncedVelocity |> Vec.mapMagnitude ((*) bounceFactor)
+
+    else
+        newBouncedVelocity

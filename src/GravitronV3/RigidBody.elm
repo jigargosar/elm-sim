c3d4ea9acab8d39713819e0949da553b715b1fac
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-13 13:37:34 +0530

    GravitronV3-2: re modeling

--- src/GravitronV3/RigidBody.elm
+++ src/GravitronV3/RigidBody.elm
@@ -1,4 +1,4 @@
-module GravitronV3.RigidBody exposing (..)
+module GravitronV3.RigidBody exposing (RigidBody, updateVelocity)
 
 import GravitronV3.Point exposing (Point)
 import GravitronV3.Vec exposing (Vec)
@@ -9,3 +9,8 @@ type alias RigidBody a =
         | position : Point
         , velocity : Vec
     }
+
+
+updateVelocity : (RigidBody a -> Vec) -> RigidBody a -> RigidBody a
+updateVelocity func model =
+    { model | velocity = func model }

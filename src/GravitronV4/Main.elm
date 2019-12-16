b0f8dbc8563b82a92d463dc7b242cb273991b48c
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-16 17:01:28 +0530

    GravitronV4: re model ;)

--- src/GravitronV4/Main.elm
+++ src/GravitronV4/Main.elm
@@ -9,13 +9,23 @@ type alias Player =
     }
 
 
+type alias Turret =
+    { x : Number
+    , y : Number
+    }
+
+
 type alias Memory =
-    { player : Player }
+    { player : Player
+    , turrets : List Turret
+    }
 
 
 viewMemory : Computer -> Memory -> List Shape
-viewMemory _ { player } =
-    [ viewPlayer player ]
+viewMemory _ { player, turrets } =
+    [ viewPlayer player
+    , viewTurrets turrets
+    ]
 
 
 viewPlayer : Player -> Shape
@@ -24,13 +34,37 @@ viewPlayer { x, y } =
         |> move x y
 
 
+viewTurrets : List Turret -> Shape
+viewTurrets =
+    let
+        viewTurret { x, y } =
+            circle red 25
+                |> move x y
+    in
+    List.map viewTurret >> group
+
+
 updateMemory _ =
     identity
 
 
 initialMemory : Memory
 initialMemory =
-    { player = Player 0 0 }
+    { player = Player 0 0
+    , turrets = initTurrets [ ( 1, 1 ) ]
+    }
+
+
+initTurrets : List ( Number, Number ) -> List Turret
+initTurrets =
+    let
+        factor =
+            150
+
+        initTurret ( x, y ) =
+            Turret (x * factor) (y * factor)
+    in
+    List.map initTurret
 
 
 main =

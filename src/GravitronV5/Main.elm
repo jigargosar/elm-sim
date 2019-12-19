affd64bdd1eceef7b815c34734c704144b74ec82
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-19 15:32:46 +0530

    GravitronV5: modeling

--- src/GravitronV5/Main.elm
+++ src/GravitronV5/Main.elm
@@ -3,13 +3,52 @@ module GravitronV5.Main exposing (main)
 import Playground exposing (..)
 
 
+type Id
+    = Id Int
+
+
+type Tag
+    = Player
+
+
+type Data
+    = Position Number Number
+    | Radius Number
+    | Velocity Number Number
+    | PrimaryColour Color
+    | PrimaryTag Tag
+    | TriggerTimer Int Int
+
+
+type Behaviour
+    = BounceOffScreen Number
+    | RandomWalker
+    | ShootOnTriggerTimer
+    | GravitateTo Tag
+    | MoveByVelocity
+
+
+type Actor
+    = Actor Id (List Data) (List Behaviour)
+
+
 type alias Mem =
-    {}
+    { actors : List Actor
+    , nextId : Int
+    }
+
+
+initialPlayer id =
+    Actor id
+        [ Position 0 0, Radius 15, PrimaryColour green, PrimaryTag Player ]
+        [ RandomWalker, BounceOffScreen 1, MoveByVelocity ]
 
 
 initialMemory : Mem
 initialMemory =
-    {}
+    { actors = [ initialPlayer (Id 0) ]
+    , nextId = 100
+    }
 
 
 updateMemory : Computer -> Mem -> Mem

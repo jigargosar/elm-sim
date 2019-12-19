46f01ad7dcda2557ab8d972698e49efac101b5cd
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-19 16:31:05 +0530

    GravitronV5: modeling

--- src/GravitronV5/Main.elm
+++ src/GravitronV5/Main.elm
@@ -1,37 +1,32 @@
 module GravitronV5.Main exposing (main)
 
-import Basics.Extra exposing (uncurry)
-import List.Extra
 import Playground exposing (..)
 
 
 type Id
-    = Id Int
+    = Id ActorId Int
 
 
-type Tag
+type ActorId
     = Player
-
-
-type Data
-    = Position Number Number
-    | Radius Number
-    | Velocity Number Number
-    | PrimaryColour Color
-    | PrimaryTag Tag
-    | TriggerTimer Int Int
 
 
 type Behaviour
     = BounceOffScreen Number
     | RandomWalker
-    | ShootOnTriggerTimer
-    | GravitateTo Tag
     | MoveByVelocity
 
 
+type Point
+    = Point Number Number
+
+
+type alias Data =
+    { pos : Point }
+
+
 type Actor
-    = Actor Id (List Data) (List Behaviour)
+    = Actor Id Data (List Behaviour)
 
 
 type alias Mem =
@@ -42,13 +37,13 @@ type alias Mem =
 
 initialPlayer id =
     Actor id
-        [ Position 0 0, Radius 15, PrimaryColour green, PrimaryTag Player ]
+        (Data (Point 0 0))
         [ RandomWalker, BounceOffScreen 1, MoveByVelocity ]
 
 
 initialMemory : Mem
 initialMemory =
-    { actors = [ initialPlayer (Id 0) ]
+    { actors = [ initialPlayer (Id Player 0) ]
     , nextId = 100
     }
 
@@ -60,131 +55,25 @@ updateMemory { time, screen, mouse } mem =
 
 updateActor : Actor -> Actor
 updateActor =
-    let
-        reducer d b =
-            case b of
-                RandomWalker ->
-                    ( d, b )
-
-                BounceOffScreen number ->
-                    ( d, b )
-
-                MoveByVelocity ->
-                    ( setPosition 10 10 d, b )
-
-                _ ->
-                    ( d, b )
-    in
-    \(Actor id d b) ->
-        List.Extra.mapAccuml reducer d b
-            |> uncurry (Actor id)
+    identity
 
 
 viewMemory : Computer -> Mem -> List Shape
 viewMemory _ { actors } =
-    List.filterMap viewActor actors
+    List.map viewActor actors
 
 
-viewActor : Actor -> Maybe Shape
-viewActor (Actor _ d _) =
-    getPrimaryTag d
-        |> Maybe.andThen (viewPrimaryTag d)
-
-
-viewPrimaryTag : List Data -> Tag -> Maybe Shape
-viewPrimaryTag d tag =
+viewActor : Actor -> Shape
+viewActor (Actor (Id tag _) data _) =
     case tag of
         Player ->
-            Maybe.map2 circle (getPrimaryColor d) (getRadius d)
-                |> Maybe.andThen (moveShapeByPosition d)
+            circle green 100
+                |> move data.pos
 
 
-moveShapeByPosition : List Data -> Shape -> Maybe Shape
-moveShapeByPosition d s =
-    getPosition d |> Maybe.map (\( x, y ) -> move x y s)
-
-
-getPrimaryTag : List Data -> Maybe Tag
-getPrimaryTag =
-    let
-        reducer d answer =
-            case ( answer, d ) of
-                ( Just _, _ ) ->
-                    answer
-
-                ( Nothing, PrimaryTag tag ) ->
-                    Just tag
-
-                _ ->
-                    answer
-    in
-    List.foldl reducer Nothing
-
-
-getPrimaryColor : List Data -> Maybe Color
-getPrimaryColor =
-    let
-        reducer d answer =
-            case ( answer, d ) of
-                ( Just _, _ ) ->
-                    answer
-
-                ( Nothing, PrimaryColour color ) ->
-                    Just color
-
-                _ ->
-                    answer
-    in
-    List.foldl reducer Nothing
-
-
-getRadius : List Data -> Maybe Number
-getRadius =
-    let
-        reducer d answer =
-            case ( answer, d ) of
-                ( Just _, _ ) ->
-                    answer
-
-                ( Nothing, Radius value ) ->
-                    Just value
-
-                _ ->
-                    answer
-    in
-    List.foldl reducer Nothing
-
-
-getPosition : List Data -> Maybe ( Number, Number )
-getPosition =
-    let
-        reducer d answer =
-            case ( answer, d ) of
-                ( Just _, _ ) ->
-                    answer
-
-                ( Nothing, Position x y ) ->
-                    Just ( x, y )
-
-                _ ->
-                    answer
-    in
-    List.foldl reducer Nothing
-
-
-setPosition : Number -> Number -> List Data -> List Data
-setPosition x y =
-    let
-        func : Data -> Data
-        func d =
-            case d of
-                Position _ _ ->
-                    Position x y
-
-                _ ->
-                    d
-    in
-    List.map func
+move : Point -> Shape -> Shape
+move (Point x y) =
+    Playground.move x y
 
 
 main =

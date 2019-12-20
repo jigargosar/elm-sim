1018bec70af2221deceae2827e014595b48eca18
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-20 16:54:43 +0530

    GravitronV5: modeling

--- src/GravitronV5/DSL.elm
+++ src/GravitronV5/DSL.elm
@@ -3,7 +3,7 @@ module GravitronV5.DSL exposing (..)
 import Playground exposing (..)
 
 
-type EntityType
+type EntityName
     = Player
     | Turret
     | Bullet
@@ -15,8 +15,55 @@ type UUID
     = UUID Int
 
 
-type Entity
-    = Entity
+type MoveBehaviour
+    = MoveBehaviour
+
+
+type Health
+    = Health
+
+
+type WeaponConfig
+    = NoWeapon
+    | FiresEvery
+
+
+type DeathBehaviour
+    = DeathBehaviour
+
+
+type alias EntityConfig =
+    { name : EntityName
+    , isSingleton : Bool
+    , moveBehaviour : MoveBehaviour
+    , bounceInScreen : Bool
+    , health : Health
+    , receivesDamageFrom : List EntityName
+    , weaponConfig : WeaponConfig
+    , onDeath : DeathBehaviour
+    }
+
+
+entityList : List EntityConfig
+entityList =
+    [ singletonEntityNamed Player
+        |> hasRandomWalkerBehaviour
+        |> hasBounceInScreenBehaviour 1
+    , entityNamed Turret
+        |> firesWeaponEvery 60
+        |> hasHP
+        |> receivesCollisionDamageFrom [ Bullet, BombBlast ]
+    , entityNamed Bullet
+        |> hasGravitateToSingletonBehaviour Player
+        |> hasBounceInScreenBehaviour 0.5
+        |> isKilledOnCollisionWith [ Bullet, BombBlast, Turret ]
+    , entityNamed TimeBomb
+        |> hasGravitateToSingletonBehaviour Player
+        |> isKilledOnCollisionWith [ Bullet, BombBlast, Turret, TimeBomb ]
+        |> onDeathSpawnsBombBlast
+    , entityNamed BombBlast
+        |> isKilledOnNextUpdate
+    ]
 
 
 type alias CommonProps =
@@ -72,25 +119,3 @@ hasBounceInScreenBehaviour bounceFriction =
 
 isKilledOnNextUpdate =
     Debug.todo "impl"
-
-
-entityList : List Entity
-entityList =
-    [ singletonEntityNamed Player
-        |> hasRandomWalkerBehaviour
-        |> hasBounceInScreenBehaviour 1
-    , entityNamed Turret
-        |> firesWeaponEvery 60
-        |> hasHP
-        |> receivesCollisionDamageFrom [ Bullet, BombBlast ]
-    , entityNamed Bullet
-        |> hasGravitateToSingletonBehaviour Player
-        |> hasBounceInScreenBehaviour 0.5
-        |> isKilledOnCollisionWith [ Bullet, BombBlast, Turret ]
-    , entityNamed TimeBomb
-        |> hasGravitateToSingletonBehaviour Player
-        |> isKilledOnCollisionWith [ Bullet, BombBlast, Turret, TimeBomb ]
-        |> onDeathSpawnsBombBlast
-    , entityNamed BombBlast
-        |> isKilledOnNextUpdate
-    ]

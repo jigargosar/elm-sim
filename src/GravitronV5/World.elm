fe78483f69c72110fb3bca853abdef3f265bd50d
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-21 19:01:09 +0530

    GravitronV5: modeling

--- src/GravitronV5/World.elm
+++ src/GravitronV5/World.elm
@@ -1,6 +1,6 @@
 module GravitronV5.World exposing (Entity, World, WorldConfig, init, toList, update)
 
-import GravitronV5.EntityConfig as EntityConfig exposing (EntityConfig, Move(..), Step(..))
+import GravitronV5.EntityConfig as EC exposing (EntityConfig, Move(..), Step(..))
 import Playground exposing (Color, Computer, Number, wave)
 
 
@@ -43,21 +43,44 @@ updateEntity { screen, time } =
                 _ ->
                     e
 
+        prependStep : Step name -> Entity name -> Entity name
+        prependStep step e =
+            { e | steps = step :: e.steps }
+
         stepEntity : Step name -> Entity name -> Entity name
         stepEntity step e =
             case step of
                 Move move ->
-                    stepMove move e
+                    stepMove move e |> prependStep step
 
-                _ ->
-                    e
+                Fire r ->
+                    let
+                        newR =
+                            if r.elapsed > r.every then
+                                { r | elapsed = 0 }
+
+                            else
+                                { r | elapsed = r.elapsed + 1 }
+                    in
+                    prependStep (Fire newR) e
+
+        revStep : Entity name -> Entity name
+        revStep e =
+            { e | steps = List.reverse e.steps }
     in
-    \e -> List.foldl stepEntity e e.step
+    \e ->
+        List.foldl stepEntity { e | steps = [] } e.steps
+            |> revStep
 
 
 withXY : ( Number, Number ) -> { c | x : Number, y : Number } -> { c | x : Number, y : Number }
 withXY ( x, y ) e =
     { e | x = x, y = y }
+
+
+type Step name
+    = Move (EC.Move name)
+    | Fire { every : Int, elapsed : Int, name : name }
 
 
 type alias Entity name =
@@ -68,14 +91,14 @@ type alias Entity name =
     , vx : Number
     , vy : Number
     , color : Color
-    , step : List (EntityConfig.Step name)
+    , steps : List (Step name)
     }
 
 
 fromConfig : EntityConfig name -> Entity name
 fromConfig =
     let
-        fromConfigRec : EntityConfig.Rec name -> Entity name
+        fromConfigRec : EC.Rec name -> Entity name
         fromConfigRec { name, x, y, r, vx, vy, color, step } =
             { name = name
             , x = x
@@ -84,10 +107,20 @@ fromConfig =
             , vx = vx
             , vy = vy
             , color = color
-            , step = step
+            , steps =
+                List.map
+                    (\s ->
+                        case s of
+                            EC.Move m ->
+                                Move m
+
+                            EC.Fire n ->
+                                Fire { every = 60, name = n, elapsed = 0 }
+                    )
+                    step
             }
     in
-    EntityConfig.toRec >> fromConfigRec
+    EC.toRec >> fromConfigRec
 
 
 toList : WorldConfig name -> World name -> List (Entity name)

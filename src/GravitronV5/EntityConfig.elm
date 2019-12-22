a83ba155966c7a54b738c3827756fdfc8ce52bb7
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-22 15:13:39 +0530

    GravitronV5: modeling

--- src/GravitronV5/EntityConfig.elm
+++ src/GravitronV5/EntityConfig.elm
@@ -48,6 +48,7 @@ type alias Rec =
     , vy : Number
     , color : Color
     , maxHP : Int
+    , spawnDuration : Int
     , preSteps : List PreStep
     , steps : List Step
     , deathSteps : List Death
@@ -67,6 +68,7 @@ named name func =
             , vy = 0
             , color = blue
             , maxHP = 1
+            , spawnDuration = 0
             , preSteps = []
             , steps = []
             , deathSteps = []

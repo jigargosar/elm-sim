663e7a700059f12638277ea22d10dce0e901af24
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-22 17:03:36 +0530

    GravitronV5: modeling

--- src/GravitronV5/Step.elm
+++ src/GravitronV5/Step.elm
@@ -8,3 +8,24 @@ import Playground exposing (Number)
 type Step
     = Move EC.Move
     | Fire { every : Int, named : Name, towards : Name, speed : Number } Int
+
+
+perform :
+    { a
+        | onMove : EC.Move -> ( b, EC.Move )
+        , onFire : { every : Int, named : Name, towards : Name, speed : Number } -> Int -> ( b, Int )
+    }
+    -> Step
+    -> ( b, Step )
+perform { onMove, onFire } step =
+    let
+        ( response, newStep ) =
+            case step of
+                Move m ->
+                    onMove m |> Tuple.mapSecond Move
+
+                Fire conf elapsed ->
+                    onFire conf elapsed
+                        |> Tuple.mapSecond (Fire conf)
+    in
+    ( response, newStep )

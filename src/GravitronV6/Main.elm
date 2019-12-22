666e97ec0e1b2c364b766b0f9938d7cef61c1bf6
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-22 17:41:07 +0530

    GravitronV6: modeling

--- src/GravitronV6/Main.elm
+++ src/GravitronV6/Main.elm
@@ -1,18 +1,26 @@
 module GravitronV6.Main exposing (main)
 
+import GravitronV6.World as World exposing (World)
 import Playground exposing (..)
 
 
+init : World
 init =
-    {}
+    World.init []
 
 
 update _ =
     identity
 
 
-view _ _ =
-    []
+view : Computer -> World -> List Shape
+view _ =
+    World.toList >> List.map viewEntity
+
+
+viewEntity e =
+    circle e.color e.r
+        |> move e.x e.y
 
 
 main =

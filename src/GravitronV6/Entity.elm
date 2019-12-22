be6eb74d77ecb5ee949e307fd0132182e9253f99
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-22 17:31:53 +0530

    GravitronV6: modeling

--- src/GravitronV6/Entity.elm
+++ src/GravitronV6/Entity.elm
@@ -1,6 +1,24 @@
 module GravitronV6.Entity exposing (..)
 
-import Playground exposing (Color, Number)
+import Playground exposing (Color, Number, blue)
+
+
+type PreStep
+    = PreStep
+
+
+type AliveStep
+    = AliveStep
+
+
+type DeathStep
+    = DeathStep
+
+
+type Phase
+    = Spawning
+    | Alive
+    | Dying
 
 
 type alias Entity =
@@ -14,4 +32,31 @@ type alias Entity =
     , vy : Number
     , maxHP : Number
     , currentHP : Number
+    , preSteps : List PreStep
+    , aliveSteps : List AliveStep
+    , deathSteps : List DeathStep
+    , phase : Phase
+    }
+
+
+invalidId =
+    -1
+
+
+default : Entity
+default =
+    { id = invalidId
+    , name = "Unknown"
+    , x = 0
+    , y = 0
+    , r = 100
+    , color = blue
+    , vx = 0
+    , vy = 0
+    , maxHP = 1
+    , currentHP = 1
+    , preSteps = []
+    , aliveSteps = []
+    , deathSteps = []
+    , phase = Alive
     }

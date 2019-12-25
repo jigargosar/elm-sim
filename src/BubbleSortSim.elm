e4aa7278ef8540e3f98388947a18b3388c65cbe3
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-25 15:09:02 +0530

    BubbleSortSim.elm

--- src/BubbleSortSim.elm
+++ src/BubbleSortSim.elm
@@ -1,18 +1,66 @@
 module BubbleSortSim exposing (main)
 
+import List.Extra
 import Playground exposing (..)
 
 
-viewNums nums =
-    let
-        strRep =
-            List.map String.fromInt nums |> String.join ", "
-    in
-    words black <| strRep
+numsToString nums =
+    List.map String.fromInt nums |> String.join ", "
 
 
 customSort nums =
     List.sort nums
+
+
+type alias Model =
+    { input : List Int
+    , out : List Int
+    , len : Int
+    , idx : Int
+    , wasSwapMade : Bool
+    , sorted : Bool
+    }
+
+
+init : List Int -> Model
+init nums =
+    Model nums nums (List.length nums) 0 False False
+
+
+step : Model -> Model
+step m =
+    if m.sorted then
+        m
+
+    else if m.idx >= m.len - 1 then
+        if m.wasSwapMade then
+            { m | idx = 0, wasSwapMade = False }
+                |> performSwap
+
+        else
+            { m | sorted = True }
+
+    else
+        performSwap m
+
+
+performSwap : Model -> Model
+performSwap m =
+    let
+        newM =
+            Maybe.map2
+                (\a b ->
+                    if a <= b then
+                        m
+
+                    else
+                        { m | out = List.Extra.swapAt m.idx (m.idx + 1) m.out, wasSwapMade = True }
+                )
+                (List.Extra.getAt m.idx m.out)
+                (List.Extra.getAt (m.idx + 1) m.out)
+                |> Maybe.withDefault m
+    in
+    { newM | idx = m.idx + 1 }
 
 
 main =
@@ -23,12 +71,29 @@ main =
         sortedNums =
             List.sort nums
 
-        quickSortNums =
-            customSort nums
+        bubbleSortNums =
+            init nums
+                |> step
+                |> step
+                |> step
+                |> step
+                |> .out
+
+        isCorrect =
+            sortedNums == bubbleSortNums
     in
     picture
-        [ viewNums nums
+        [ words black ("Input:  " ++ numsToString nums)
             |> moveUp 20
-        , viewNums sortedNums
+        , words lightBlue (numsToString sortedNums)
             |> moveDown 20
+        , words
+            (if isCorrect then
+                green
+
+             else
+                red
+            )
+            (numsToString bubbleSortNums)
+            |> moveDown 60
         ]

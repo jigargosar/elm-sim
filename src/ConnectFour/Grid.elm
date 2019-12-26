cc50f12352ec76e1e720beeb9f92a0436a85f3b1
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-26 21:48:45 +0530

    GravitronV6: modeling

--- src/ConnectFour/Grid.elm
+++ src/ConnectFour/Grid.elm
@@ -31,8 +31,8 @@ type Grid
 type alias GridModel =
     { width : Int
     , height : Int
-    , cords : List ( Int, Int )
-    , cells : Dict ( Int, Int ) Cell
+    , cords : List Cord
+    , cells : Dict Cord Cell
     }
 
 
@@ -53,21 +53,21 @@ clampCord =
 empty : Int -> Int -> Grid
 empty w h =
     let
-        cords : List ( Int, Int )
+        cords : List Cord
         cords =
             List.range 0 (w - 1)
                 |> List.map (\x -> List.range 0 (h - 1) |> List.map (\y -> ( x, y )))
                 |> List.concat
                 |> List.sort
 
-        setEmpty : ( Int, Int ) -> Dict ( Int, Int ) Cell -> Dict ( Int, Int ) Cell
+        setEmpty : Cord -> Dict Cord Cell -> Dict Cord Cell
         setEmpty cord =
             Dict.insert cord Empty
     in
     Grid { width = w, height = h, cords = cords, cells = List.foldl setEmpty Dict.empty cords }
 
 
-get : ( Int, Int ) -> Grid -> Maybe Cell
+get : Cord -> Grid -> Maybe Cell
 get cord ((Grid { cells }) as grid) =
     if isValidGridCord cord grid then
         Dict.get cord cells
@@ -81,17 +81,17 @@ map func =
     unwrap >> func >> Grid
 
 
-set : ( Int, Int ) -> Cell -> Grid -> Grid
+set : Cord -> Cell -> Grid -> Grid
 set cord cell =
     map <| \grid -> { grid | cells = Dict.insert cord cell grid.cells }
 
 
-update : ( Int, Int ) -> (Cell -> Cell) -> Grid -> Grid
+update : Cord -> (Cell -> Cell) -> Grid -> Grid
 update cord func =
     map <| \grid -> { grid | cells = Dict.update cord (Maybe.map func) grid.cells }
 
 
-validateGridCord : ( Int, Int ) -> Grid -> Maybe ( Int, Int )
+validateGridCord : Cord -> Grid -> Maybe Cord
 validateGridCord cord grid =
     if isValidGridCord cord grid then
         Just cord
@@ -100,7 +100,7 @@ validateGridCord cord grid =
         Nothing
 
 
-isValidGridCord : ( Int, Int ) -> Grid -> Bool
+isValidGridCord : Cord -> Grid -> Bool
 isValidGridCord ( x, y ) =
     unwrap
         >> (\grid ->
@@ -112,7 +112,7 @@ isValidGridCord ( x, y ) =
            )
 
 
-xEq : Int -> ( Int, Int ) -> Bool
+xEq : Int -> Cord -> Bool
 xEq column ( x, _ ) =
     column == x
 
@@ -127,7 +127,7 @@ setAtFirstNonEmptyYOfX x cell grid =
             grid
 
 
-getFirstNonEmptyCordWhereXEq : Int -> Grid -> Maybe ( Int, Int )
+getFirstNonEmptyCordWhereXEq : Int -> Grid -> Maybe Cord
 getFirstNonEmptyCordWhereXEq x grid =
     let
         pred cord cellAtCord =
@@ -136,21 +136,26 @@ getFirstNonEmptyCordWhereXEq x grid =
     findCord pred grid
 
 
-find : (( Int, Int ) -> Cell -> Bool) -> Grid -> Maybe ( ( Int, Int ), Cell )
+find : (( Int, Int ) -> Cell -> Bool) -> Grid -> Maybe ( Cord, Cell )
 find pred =
     unwrap >> .cells >> Dict.Extra.find pred
 
 
-findCord : (( Int, Int ) -> Cell -> Bool) -> Grid -> Maybe ( Int, Int )
+findCord : (( Int, Int ) -> Cell -> Bool) -> Grid -> Maybe Cord
 findCord pred grid =
     find pred grid |> Maybe.map Tuple.first
 
 
-cords__ : Grid -> List ( Int, Int )
+cords__ : Grid -> List Cord
 cords__ =
     unwrap >> .cords
 
 
-dimensions : Grid -> ( Int, Int )
+toList : Grid -> List ( ( Int, Int ), Cell )
+toList =
+    unwrap >> .cells >> Dict.toList
+
+
+dimensions : Grid -> Cord
 dimensions =
     unwrap >> (\{ width, height } -> ( width, height ))

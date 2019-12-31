8b2a56872758fb31bdfa5317b17fcfc978fb6013
Jigar Gosar <jigar.gosar@gmail.com>
2019-12-31 15:53:39 +0530

    ConnectFour: AI

--- src/ConnectFour/GridShape.elm
+++ src/ConnectFour/GridShape.elm
@@ -11,50 +11,52 @@ withCellSize cellSize grid =
         ( widthPx, heightPx ) =
             ( Grid.width grid, Grid.height grid )
                 |> mapEach (toFloat >> (*) cellSize)
-
-        viewCell ( ( x, y ), cell ) =
-            let
-                cellR =
-                    cellSize / 2
-
-                bgR =
-                    cellR * 0.8
-
-                cellBackgroundShape =
-                    circle white bgR
-
-                coinR =
-                    bgR * 0.8
-
-                coinShapeFromColor color =
-                    circle color coinR
-
-                maybeCoinColor : Maybe Color
-                maybeCoinColor =
-                    cell
-                        |> Maybe.map
-                            (\coin ->
-                                case coin of
-                                    Grid.Red ->
-                                        red
-
-                                    Grid.Yellow ->
-                                        blue
-                            )
-
-                shape =
-                    group
-                        [ cellBackgroundShape
-                        , maybeCoinColor
-                            |> Maybe.map coinShapeFromColor
-                            |> Maybe.withDefault (group [])
-                        ]
-            in
-            shape |> move (toFloat x * cellSize) (toFloat y * cellSize)
     in
     group
         [ rectangle black widthPx heightPx
-        , List.map viewCell (Grid.toCellList grid)
+        , List.map (cellToShape cellSize) (Grid.toCellList grid)
             |> group
             |> move (-widthPx / 2 + cellSize / 2) (-heightPx / 2 + cellSize / 2)
         ]
+
+
+cellToShape : Float -> ( ( Int, Int ), Maybe Grid.Coin ) -> Shape
+cellToShape cellSize ( ( x, y ), cell ) =
+    let
+        cellR =
+            cellSize / 2
+
+        bgR =
+            cellR * 0.8
+
+        cellBackgroundShape =
+            circle white bgR
+
+        coinR =
+            bgR * 0.8
+
+        coinShapeFromColor color =
+            circle color coinR
+
+        maybeCoinColor : Maybe Color
+        maybeCoinColor =
+            cell
+                |> Maybe.map
+                    (\coin ->
+                        case coin of
+                            Grid.Red ->
+                                red
+
+                            Grid.Yellow ->
+                                blue
+                    )
+
+        shape =
+            group
+                [ cellBackgroundShape
+                , maybeCoinColor
+                    |> Maybe.map coinShapeFromColor
+                    |> Maybe.withDefault (group [])
+                ]
+    in
+    shape |> move (toFloat x * cellSize) (toFloat y * cellSize)

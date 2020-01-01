f3f0b17e65de84c5219dc8d463699402302504e7
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-01 15:12:02 +0530

    ConnectFour v2: local 2 player

--- src/ConnectFourV2/Main.elm
+++ src/ConnectFourV2/Main.elm
@@ -1,6 +1,9 @@
 module ConnectFourV2.Main exposing (main)
 
+import Array exposing (Array)
 import Dict exposing (Dict)
+import Dict.Extra
+import List.Extra
 import Playground exposing (..)
 
 
@@ -23,7 +26,51 @@ type alias Mem =
 
 initialMemory : Mem
 initialMemory =
+    let
+        _ =
+            Dict.Extra.frequencies [ 1, 2, 1, 1, 12, 1, 1 ]
+                |> Debug.log "freq"
+
+        _ =
+            initBoard 1 1 Blue [ 0, 0 ]
+                |> Debug.log "board"
+    in
     { coin = Blue, grid = Dict.empty, width = 7, height = 6 }
+
+
+type ValidBoard
+    = ValidBoard Int Int Coin (List Int)
+
+
+initBoard : Int -> Int -> Coin -> List Int -> Maybe ValidBoard
+initBoard w h coin moves =
+    let
+        foo : ( List Int, List Int )
+        foo =
+            Dict.Extra.frequencies moves
+                |> Dict.toList
+                |> List.unzip
+                |> Debug.log "foo"
+
+        ( columnIndices, columnLengths ) =
+            foo
+
+        isValidIdx len idx =
+            idx >= 0 && idx < len
+
+        areMovesValid =
+            List.all (isValidIdx w) columnIndices
+                && List.all ((+) -1 >> isValidIdx h) columnLengths
+    in
+    if areMovesValid then
+        ValidBoard w h coin moves |> Just
+
+    else
+        Nothing
+
+
+
+-- makeMove column mem =
 
 
 updateMemory : Computer -> Mem -> Mem

bb8cf74eb31aba99b09016d002af706a57321bf9
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-01 20:50:41 +0530

    ConnectFour v2: extracting Board View

--- src/ConnectFourV2/Main.elm
+++ src/ConnectFourV2/Main.elm
@@ -1,6 +1,7 @@
 module ConnectFourV2.Main exposing (main)
 
 import ConnectFourV2.Board as Board exposing (Board)
+import ConnectFourV2.BoardShape as BoardShape
 import ConnectFourV2.Coin as Coin exposing (Coin)
 import Dict exposing (Dict)
 import List.Extra
@@ -52,7 +53,7 @@ viewMemory _ model =
                 moves =
                     Board.toList mem.board
             in
-            [ toBoardShape 50 w h (toAllCells w h moves) ]
+            [ BoardShape.toBoardShape 50 w h mem.board ]
 
         Err msg ->
             [ words black <| "Error: " ++ msg ]
@@ -60,113 +61,6 @@ viewMemory _ model =
 
 
 -- ViewModel
-
-
-toAllCells : Int -> Int -> List Int -> List ( ( Int, Int ), Maybe Coin )
-toAllCells w h =
-    toPositionCoinPairs
-        >> Dict.fromList
-        >> (\dict ->
-                mapPositionsFromWH w
-                    h
-                    (\pos ->
-                        ( pos, Dict.get pos dict )
-                    )
-           )
-
-
-toPositionCoinPairs : List Int -> List ( ( Int, Int ), Coin )
-toPositionCoinPairs =
-    let
-        incLengthOfColumn : Int -> Dict Int Int -> Dict Int Int
-        incLengthOfColumn column =
-            Dict.update column <|
-                \maybeLength ->
-                    case maybeLength of
-                        Just length ->
-                            Just (length + 1)
-
-                        Nothing ->
-                            Just 1
-
-        lengthOfColumn : Int -> Dict Int Int -> Int
-        lengthOfColumn column lookup =
-            Dict.get column lookup |> Maybe.withDefault 0
-
-        reducer : ( Coin, Dict Int Int ) -> Int -> ( ( Coin, Dict Int Int ), ( ( Int, Int ), Coin ) )
-        reducer ( coin, lenLookup ) column =
-            ( ( Coin.flip coin, incLengthOfColumn column lenLookup )
-            , ( ( column, lengthOfColumn column lenLookup ), coin )
-            )
-    in
-    List.Extra.mapAccuml reducer ( Coin.Blue, Dict.empty )
-        >> Tuple.second
-        >> List.reverse
-
-
-toPositions : Int -> Int -> List ( Int, Int )
-toPositions w h =
-    List.Extra.initialize w
-        (\x -> List.Extra.initialize h (\y -> ( x, y )))
-        |> List.concat
-
-
-mapPositionsFromWH : Int -> Int -> (( Int, Int ) -> b) -> List b
-mapPositionsFromWH w h func =
-    toPositions w h |> List.map func
-
-
-
--- View
-
-
-coinToColor : Coin -> Color
-coinToColor coin =
-    case coin of
-        Coin.Blue ->
-            blue
-
-        Coin.Red ->
-            red
-
-
-toBoardShape : Float -> Int -> Int -> List ( ( Int, Int ), Maybe Coin ) -> Shape
-toBoardShape cellSize w h list =
-    let
-        ( widthPx, heightPx ) =
-            ( toFloat w * cellSize, toFloat h * cellSize )
-
-        moveCell ( x, y ) =
-            let
-                leftOffset =
-                    -widthPx / 2 + cellSize / 2
-
-                bottomOffset =
-                    -heightPx / 2 + cellSize / 2
-            in
-            move (toFloat x * cellSize + leftOffset)
-                (toFloat y * cellSize + bottomOffset)
-
-        toCellShape : Int -> ( ( Int, Int ), Maybe Coin ) -> Shape
-        toCellShape idx ( position, cell ) =
-            (circle white (cellSize / 2 * 0.8)
-                :: (case cell of
-                        Just coin ->
-                            [ circle (coinToColor coin) (cellSize / 2 * 0.7)
-                            , words white (String.fromInt idx)
-                            ]
-
-                        Nothing ->
-                            []
-                   )
-            )
-                |> group
-                |> moveCell position
-    in
-    group
-        [ rectangle black widthPx heightPx
-        , List.indexedMap toCellShape list |> group
-        ]
 
 
 main =

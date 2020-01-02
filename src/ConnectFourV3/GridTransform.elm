97aca61cb92c71f59bdaf01f30e1e8256aaad6cb
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-02 17:45:34 +0530

    ConnectFour v2: insert coin

--- src/ConnectFourV3/GridTransform.elm
+++ src/ConnectFourV3/GridTransform.elm
@@ -1,5 +1,6 @@
 module ConnectFourV3.GridTransform exposing
     ( GridTransform
+    , cellSize
     , fromScreen
     , fromScreenX
     , fromScreenY
@@ -15,30 +16,30 @@ type GridTransform
 
 
 init : Float -> { a | columns : Int, rows : Int } -> GridTransform
-init cellSize { columns, rows } =
-    GridTransform cellSize
-        (-(toFloat columns * cellSize) / 2 + cellSize / 2)
-        (-(toFloat rows * cellSize) / 2 + cellSize / 2)
+init cs { columns, rows } =
+    GridTransform cs
+        (-(toFloat columns * cs) / 2 + cs / 2)
+        (-(toFloat rows * cs) / 2 + cs / 2)
 
 
 toScreenX : Int -> GridTransform -> Float
-toScreenX column (GridTransform cellSize dx _) =
-    (toFloat column * cellSize) + dx
+toScreenX column (GridTransform cs dx _) =
+    (toFloat column * cs) + dx
 
 
 toScreenY : Int -> GridTransform -> Float
-toScreenY row (GridTransform cellSize _ dy) =
-    (toFloat row * cellSize) + dy
+toScreenY row (GridTransform cs _ dy) =
+    (toFloat row * cs) + dy
 
 
 fromScreenX : Float -> GridTransform -> Int
-fromScreenX x (GridTransform cellSize dx _) =
-    (x - dx) / cellSize |> round
+fromScreenX x (GridTransform cs dx _) =
+    (x - dx) / cs |> round
 
 
 fromScreenY : Float -> GridTransform -> Int
-fromScreenY y (GridTransform cellSize _ dy) =
-    (y - dy) / cellSize |> round
+fromScreenY y (GridTransform cs _ dy) =
+    (y - dy) / cs |> round
 
 
 toScreen : ( Int, Int ) -> GridTransform -> ( Float, Float )
@@ -49,3 +50,8 @@ toScreen ( column, row ) model =
 fromScreen : ( Float, Float ) -> GridTransform -> ( Int, Int )
 fromScreen ( x, y ) model =
     ( fromScreenX x model, fromScreenY y model )
+
+
+cellSize : GridTransform -> Float
+cellSize (GridTransform cs _ _) =
+    cs

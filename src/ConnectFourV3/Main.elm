2c5365cc4d7597c88abf8cf59f2bcca4042012d4
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-02 13:43:43 +0530

    ConnectFour v2: draw board

--- src/ConnectFourV3/Main.elm
+++ src/ConnectFourV3/Main.elm
@@ -40,23 +40,74 @@ updateMemory _ mem =
     mem
 
 
+type alias BoardView =
+    { cellRadius : Float
+    , cellSize : Float
+    , width : Float
+    , height : Float
+    , dx : Float
+    , dy : Float
+    }
+
+
+toBoardView : Float -> BoardView
+toBoardView cellSize =
+    let
+        width =
+            toFloat columns * cellSize
+
+        height =
+            toFloat rows * cellSize
+
+        cellRadius =
+            cellSize / 2
+    in
+    { cellRadius = cellRadius
+    , cellSize = cellSize
+    , width = width
+    , height = height
+    , dx = -width / 2 + cellRadius
+    , dy = -height / 2 + cellRadius
+    }
+
+
+coinToColor : Coin -> Color
+coinToColor coin =
+    case coin of
+        Red ->
+            red
+
+        Blue ->
+            blue
+
+
 viewMemory : Computer -> Mem -> List Shape
 viewMemory _ { board } =
     let
-        cellRadius =
-            25
+        { width, height, cellRadius, cellSize, dx, dy } =
+            toBoardView 50
 
-        cellSize =
-            cellRadius * 2
+        allBoardPositions : List ( Int, Int )
+        allBoardPositions =
+            List.range 0 (columns - 1)
+                |> List.concatMap (\x -> List.range 0 (rows - 1) |> List.map (Tuple.pair x))
 
-        width =
-            columns * cellSize
+        viewCellAt ( x, y ) =
+            group
+                [ circle white (cellRadius * 0.8)
+                , case Dict.get ( x, y ) board of
+                    Just coin ->
+                        circle (coinToColor coin) (cellRadius * 0.8)
 
-        height =
-            rows * cellSize
+                    Nothing ->
+                        group []
+                ]
+                |> move (toFloat x * cellSize + dx) (toFloat y * cellSize + dy)
     in
     [ group
         [ rectangle black width height
+        , List.map viewCellAt allBoardPositions
+            |> group
         ]
     ]
 

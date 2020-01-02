a199acb19bd9e48a255a2100c7b6ce3b3c8870e0
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-02 14:48:43 +0530

    ConnectFour v2: insert coin

--- src/ConnectFourV3/Main.elm
+++ src/ConnectFourV3/Main.elm
@@ -189,13 +189,13 @@ viewMemory _ { board } =
         viewCellBackgroundAt position =
             cellBackgroundShape |> moveCellShape position
 
-        viewCoinAt ( pos, coin ) =
+        viewBoardCoin ( pos, coin ) =
             coinToShape coin |> moveCellShape pos
     in
     [ group
         [ rectangle black width height
         , List.map viewCellBackgroundAt allBoardPositions |> group
-        , List.map viewCoinAt (Dict.toList board) |> group
+        , List.map viewBoardCoin (Dict.toList board) |> group
         ]
     ]
 

1af0e0ac069c9c7a00589f72bf8169d4dc1e878a
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-17 11:24:00 +0530

    MirrorPuzzle.elm v2: scene models

--- src/MirrorPuzzleV2/Main.elm
+++ src/MirrorPuzzleV2/Main.elm
@@ -2,248 +2,9 @@ module MirrorPuzzleV2.Main exposing (main)
 
 import List.Extra
 import MirrorPuzzleV2.Box as Box exposing (Box)
-import Number2 as NT exposing (Float2, Int2)
+import MirrorPuzzleV2.PuzzleGrid as PuzzleGrid exposing (PuzzleGrid)
 import Playground exposing (..)
-import Playground.CellTransform as CT exposing (CellTransform)
-import Playground.Direction8 as Dir exposing (Direction8)
 import Playground.Extra exposing (..)
-import Playground.Grid as Grid exposing (Pos)
-import PointFree exposing (whenTrue)
-import Set exposing (Set)
-
-
-
--- Puzzle Grid Model
-
-
-type PuzzleCell
-    = Source
-    | Destination
-    | SourceWithMirror Direction8
-    | Mirror Direction8
-    | Empty
-
-
-type alias PuzzleGrid =
-    Grid.Grid PuzzleCell
-
-
-mirror : Int -> PuzzleCell
-mirror =
-    Dir.fromInt >> Mirror
-
-
-sourceWithMirror : Int -> PuzzleCell
-sourceWithMirror =
-    Dir.fromInt >> SourceWithMirror
-
-
-initialGrid : PuzzleGrid
-initialGrid =
-    let
-        insert =
-            Grid.insert
-    in
-    Grid.filled 5 5 Empty
-        |> insert ( 1, 2 ) (sourceWithMirror 1)
-        |> insert ( 2, 3 ) (mirror 7)
-        |> insert ( 3, 2 ) Destination
-        |> insert ( 4, 4 ) (sourceWithMirror -3)
-        |> insert ( 0, 0 ) Destination
-        |> insert ( 1, 1 ) (sourceWithMirror 1)
-        |> insert ( 1, 1 ) Source
-
-
-destinationSet : PuzzleGrid -> Set Pos
-destinationSet =
-    Grid.foldl (\pos cell -> whenTrue (cell == Destination) (Set.insert pos))
-        Set.empty
-
-
-isSolved : PuzzleGrid -> Bool
-isSolved grid =
-    destinationSet grid == listDestinations grid
-
-
-listDestinations : PuzzleGrid -> Set Pos
-listDestinations grid =
-    gridToLightPaths grid
-        |> List.filterMap List.head
-        |> List.foldl
-            (\pos ->
-                if Grid.get pos grid == Just Destination then
-                    Set.insert pos
-
-                else
-                    identity
-            )
-            Set.empty
-
-
-type alias LightPath =
-    List Pos
-
-
-gridToLightPaths : PuzzleGrid -> List LightPath
-gridToLightPaths grid =
-    let
-        accumLightPos : Direction8 -> Pos -> List Pos -> List Pos
-        accumLightPos dir pos acc =
-            let
-                nextPos =
-                    Dir.stepPos dir pos
-
-                accumInDir newDir =
-                    accumLightPos newDir nextPos (nextPos :: acc)
-            in
-            case Grid.get nextPos grid of
-                Nothing ->
-                    acc
-
-                Just cell ->
-                    case cell of
-                        Source ->
-                            accumInDir dir
-
-                        SourceWithMirror _ ->
-                            acc
-
-                        Destination ->
-                            nextPos :: acc
-
-                        Mirror direction ->
-                            accumInDir direction
-
-                        Empty ->
-                            accumInDir dir
-    in
-    Grid.foldl
-        (\pos cell ->
-            case cell of
-                SourceWithMirror dir ->
-                    accumLightPos dir pos [ pos ]
-                        |> (::)
-
-                _ ->
-                    identity
-        )
-        []
-        grid
-
-
-
--- Puzzle Grid View
-
-
-initCellT : Screen -> PuzzleGrid -> CellTransform
-initCellT screen grid =
-    let
-        viewD =
-            NT.scale 0.8 ( screen.width, screen.height )
-    in
-    CT.fromViewD viewD (Grid.dimensions grid)
-
-
-viewPuzzleGrid : Computer -> PuzzleGrid -> Shape
-viewPuzzleGrid { time, screen } grid =
-    let
-        ct =
-            initCellT screen grid
-
-        lightPathsShape =
-            grid
-                |> gridToLightPaths
-                |> List.map (viewPath ct)
-                |> group
-    in
-    group [ gridCellShapes time ct grid |> group, lightPathsShape ]
-
-
-
--- Puzzle Ligh Path View
-
-
-viewPath : CellTransform -> List Pos -> Shape
-viewPath ct =
-    List.map ct.toView
-        >> (\path -> List.map2 (line red 5) path (List.drop 1 path))
-        >> group
-
-
-
--- Puzzle Cell View
-
-
-gridCellShapes : Time -> CellTransform -> PuzzleGrid -> List Shape
-gridCellShapes time ct grid =
-    let
-        litDest =
-            listDestinations grid
-
-        toCellView ( pos, cell ) =
-            cellShape time ct litDest pos cell
-    in
-    grid |> Grid.toList |> List.map toCellView
-
-
-cellShape : Time -> CellTransform -> Set Int2 -> Int2 -> PuzzleCell -> Shape
-cellShape time ct litDest pos cell =
-    let
-        bg : Shape
-        bg =
-            group
-                [ rectangle black width width |> scale 0.95
-                , rectangle white width width |> scale 0.9
-                ]
-
-        width =
-            ct.cellSize
-    in
-    cellContentShapes time width litDest pos cell
-        |> List.map (scale 0.8)
-        |> (::) bg
-        |> group
-        |> move2 (ct.toView pos)
-
-
-cellContentShapes : Time -> Number -> Set comparable -> comparable -> PuzzleCell -> List Shape
-cellContentShapes time width litDest pos cell =
-    case cell of
-        Source ->
-            [ srcShape width ]
-
-        Destination ->
-            [ destinationShape time width (Set.member pos litDest) ]
-
-        SourceWithMirror dir ->
-            [ srcShape width, mirrorShape width dir ]
-
-        Mirror dir ->
-            [ mirrorShape width dir ]
-
-        Empty ->
-            []
-
-
-srcShape : Number -> Shape
-srcShape width =
-    rectangle orange width width
-
-
-mirrorShape : Float -> Direction8 -> Shape
-mirrorShape width dir =
-    group
-        [ group [ oval green (width / 2) width |> moveLeft (width / 6) ]
-            |> rotate (Dir.toDegrees dir)
-        , circle lightPurple 10
-        ]
-        |> scale 0.9
-
-
-destinationShape : Time -> Float -> Bool -> Shape
-destinationShape time width isLit =
-    circle blue (width / 2)
-        |> whenTrue isLit (blink time)
 
 
 
@@ -256,7 +17,7 @@ type alias PuzzleSceneModel =
 
 initialPuzzleScene : Scene
 initialPuzzleScene =
-    PuzzleScene { grid = initialGrid }
+    PuzzleScene { grid = PuzzleGrid.initialGrid }
 
 
 
@@ -264,35 +25,8 @@ initialPuzzleScene =
 
 
 updatePuzzleScene : Computer -> PuzzleSceneModel -> PuzzleSceneModel
-updatePuzzleScene { screen, mouse } model =
-    let
-        grid =
-            model.grid
-
-        ct =
-            initCellT screen grid
-
-        pos =
-            ct.fromView ( mouse.x, mouse.y )
-    in
-    case Grid.get pos grid of
-        Just cell ->
-            let
-                ins a =
-                    { model | grid = Grid.insert pos a grid }
-            in
-            case cell of
-                Mirror dir ->
-                    ins (Mirror (Dir.rotate 1 dir))
-
-                SourceWithMirror dir ->
-                    ins (SourceWithMirror (Dir.rotate 1 dir))
-
-                _ ->
-                    model
-
-        Nothing ->
-            model
+updatePuzzleScene computer model =
+    { model | grid = PuzzleGrid.updatePuzzleGrid computer model.grid }
 
 
 
@@ -305,8 +39,8 @@ viewPuzzleScene computer { grid } =
         { mouse, time, screen } =
             computer
     in
-    [ viewPuzzleGrid computer grid
-    , if isSolved grid then
+    [ PuzzleGrid.viewPuzzleGrid computer grid
+    , if PuzzleGrid.isSolved grid then
         words black "puzzle solved"
             |> moveY screen.top
             |> moveDown 50

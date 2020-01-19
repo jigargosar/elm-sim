b5b67167c2c792586bd1d2fb19f4be342c4109f9
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-19 17:29:15 +0530

    MirrorPuzzle.elm v2: Puzzle Levels

--- src/MirrorPuzzleV2/Levels.elm
+++ src/MirrorPuzzleV2/Levels.elm
@@ -1,35 +1,74 @@
-module MirrorPuzzleV2.Levels exposing (Levels, current, next, prev)
+module MirrorPuzzleV2.Levels exposing
+    ( Levels
+    , count
+    , current
+    , initial
+    , next
+    , prev
+    )
 
+import Cons exposing (Cons)
+import MirrorPuzzleV2.PuzzleGrid as PuzzleGrid
 import Pivot exposing (Pivot)
 import PointFree exposing (ignoreNothing)
 
 
+type alias Lvl =
+    PuzzleGrid.Model
+
+
 type Levels
-    = Levels (Pivot String)
+    = Levels (Pivot Lvl)
 
 
-init : String -> List String -> Levels
-init first rest =
+levelCons : ( PuzzleGrid.Model, List PuzzleGrid.Model )
+levelCons =
+    Cons.cons """
+         __,__,__,__,__,__,__,__
+         __,S ,M0,__,__,__,D ,__
+         __,__,__,__,__,__,__,__
+         """
+        [ """
+          __,__,__,__,__,__,__,__
+          __,S0,__,__,__,__,D ,__
+          __,__,__,__,__,__,__,__
+          """
+        ]
+        |> Cons.map PuzzleGrid.fromString
+        |> Cons.uncons
+
+
+initial : Levels
+initial =
+    let
+        ( first, rest ) =
+            levelCons
+    in
     Levels (Pivot.fromCons first rest)
 
 
-current : Levels -> String
+current : Levels -> Lvl
 current (Levels pivot) =
     Pivot.getC pivot
 
 
-prev : Levels -> ( String, Levels )
+prev : Levels -> ( Lvl, Levels )
 prev (Levels pivot) =
     ignoreNothing (Pivot.goBy -1) pivot
         |> withCurrent
 
 
-next : Levels -> ( String, Levels )
+next : Levels -> ( Lvl, Levels )
 next (Levels pivot) =
     ignoreNothing (Pivot.goBy 1) pivot
         |> withCurrent
 
 
-withCurrent : Pivot String -> ( String, Levels )
+count : Levels -> Int
+count (Levels p) =
+    Pivot.lengthA p
+
+
+withCurrent : Pivot Lvl -> ( Lvl, Levels )
 withCurrent pivot =
     ( Pivot.getC pivot, Levels pivot )

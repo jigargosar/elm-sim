699b96b17fd3a9545b13c918e7912fbcb98c6cff
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-21 16:12:07 +0530

    MirrorPuzzle v3: Puzzle TileGrid Modeling

--- src/MirrorPuzzleV3/Tile.elm
+++ src/MirrorPuzzleV3/Tile.elm
@@ -1,4 +1,4 @@
-module MirrorPuzzleV3.Tile exposing (..)
+module MirrorPuzzleV3.Tile exposing (Tile(..), rotateElement, swapElements)
 
 -- Tile
 
@@ -11,8 +11,8 @@ type Tile
     | Hole
 
 
-mapElementInTile : (Element -> Element) -> Tile -> Maybe Tile
-mapElementInTile func tile =
+mapElement : (Element -> Element) -> Tile -> Maybe Tile
+mapElement func tile =
     case tile of
         FilledContainer container element ->
             Just (FilledContainer container (func element))
@@ -21,13 +21,13 @@ mapElementInTile func tile =
             Nothing
 
 
-rotateElementInTile : Tile -> Maybe Tile
-rotateElementInTile =
-    mapElementInTile rotateElement
+rotateElement : Tile -> Maybe Tile
+rotateElement =
+    mapElement rotateElement_
 
 
-swapElementInTiles : Tile -> Tile -> Maybe ( Tile, Tile )
-swapElementInTiles dragTile dropTile =
+swapElements : Tile -> Tile -> Maybe ( Tile, Tile )
+swapElements dragTile dropTile =
     case ( dragTile, dropTile ) of
         ( FilledContainer container1 element, EmptyContainer container2 ) ->
             Just ( EmptyContainer container1, FilledContainer container2 element )
@@ -36,10 +36,25 @@ swapElementInTiles dragTile dropTile =
             Nothing
 
 
-getElementInLightSourceTile : Tile -> Maybe Element
-getElementInLightSourceTile tile =
+getElementInLightSource : Tile -> Maybe Element
+getElementInLightSource tile =
     case tile of
         FilledContainer LightSource element ->
+            Just element
+
+        _ ->
+            Nothing
+
+
+getRefractionDirections : Tile -> List Direction
+getRefractionDirections =
+    getElement >> Maybe.map getRefractionDirectionsOfElement >> Maybe.withDefault []
+
+
+getElement : Tile -> Maybe Element
+getElement tile =
+    case tile of
+        FilledContainer _ element ->
             Just element
 
         _ ->
@@ -69,6 +84,11 @@ rotateDirectionBy steps (Direction zeroToSevenInt) =
     Direction (modBy 8 (zeroToSevenInt + steps))
 
 
+oppositeDirection : Direction -> Direction
+oppositeDirection =
+    rotateDirectionBy 4
+
+
 
 -- Element
 
@@ -80,11 +100,21 @@ type alias Element =
     }
 
 
+getRefractionDirectionsOfElement : Element -> List Direction
+getRefractionDirectionsOfElement element =
+    case element.type_ of
+        Mirror ->
+            [ element.direction ]
+
+        Prism ->
+            [ element.direction, oppositeDirection element.direction ]
+
+
 rotateElementBy : Int -> Element -> Element
 rotateElementBy steps element =
     { element | direction = rotateDirectionBy steps element.direction }
 
 
-rotateElement : Element -> Element
-rotateElement =
+rotateElement_ : Element -> Element
+rotateElement_ =
     rotateElementBy 1

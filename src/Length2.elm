1da2224bc7b74cdc40ed74de117019215db4fc08
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-23 21:30:24 +0530

    MirrorPuzzle v3: Light Path Modeling

--- src/Length2.elm
+++ src/Length2.elm
@@ -1,4 +1,4 @@
-module Length2 exposing (member, toDict)
+module Length2 exposing (toDict)
 
 import Dict exposing (Dict)
 import Number2 as NT exposing (Int2)
@@ -7,13 +7,3 @@ import Number2 as NT exposing (Int2)
 toDict : (Int2 -> b) -> Int2 -> Dict Int2 b
 toDict func =
     NT.fold (\i2 -> Dict.insert i2 (func i2)) Dict.empty
-
-
-member : Int2 -> Int2 -> Bool
-member ( x, y ) ( w, h ) =
-    isIndexMemberOf w x && isIndexMemberOf h y
-
-
-isIndexMemberOf : number -> number -> Bool
-isIndexMemberOf length index =
-    index >= 0 && index <= length

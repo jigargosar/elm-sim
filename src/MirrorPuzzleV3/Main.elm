90151dab6b4121022165b031f10f6a404256f492
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-24 20:34:51 +0530

    MirrorPuzzle v3: Collage View

--- src/MirrorPuzzleV3/Main.elm
+++ src/MirrorPuzzleV3/Main.elm
@@ -1,5 +1,9 @@
 module MirrorPuzzleV3.Main exposing (main)
 
+import Collage exposing (circle, filled, rectangle, uniform)
+import Collage.Layout exposing (at, topLeft)
+import Collage.Render exposing (svg)
+import Color
 import Html exposing (Html)
 import Html.Attributes exposing (class)
 import MirrorPuzzleV3.ElGrid as ElGrid
@@ -13,6 +17,10 @@ import MirrorPuzzleV3.TileGridDemo as TileGridDemo
 main =
     Html.div [ class "pa2 inline-flex flex-wrap" ]
         [ Html.div [ class "inline-flex flex-column" ]
+            [ Html.div [ class "tc pa2" ] [ Html.text "collageDemo" ]
+            , Html.div [ class "pa4" ] [ collageDemo ]
+            ]
+        , Html.div [ class "inline-flex flex-column" ]
             [ Html.div [ class "tc pa2" ] [ Html.text "TileGrid" ]
             , TileGridDemo.viewDemo
             ]
@@ -21,3 +29,18 @@ main =
             , ElGrid.viewDemo
             ]
         ]
+
+
+collageDemo =
+    let
+        circ =
+            circle 50
+                |> filled (uniform Color.red)
+
+        rect =
+            rectangle 200 100
+                |> filled (uniform Color.blue)
+    in
+    rect
+        |> at topLeft circ
+        |> svg

9c95a89ee86437a55c2edc7919626e73d88d722e
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-25 18:43:48 +0530

    MirrorPuzzle v3: Demos.elm

--- src/MirrorPuzzleV3/Main.elm
+++ src/MirrorPuzzleV3/Main.elm
@@ -1,5 +1,7 @@
 module MirrorPuzzleV3.Main exposing (main)
 
+import Browser
+import Html exposing (Html)
 import MirrorPuzzleV3.Demos as Demos
 
 
@@ -8,4 +10,51 @@ import MirrorPuzzleV3.Demos as Demos
 
 
 main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    ()
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( (), Cmd.none )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch []
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view _ =
     Demos.main

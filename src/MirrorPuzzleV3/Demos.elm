c62c4d361397f7e4a0ff40973ad9c466bfb832ab
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-25 18:36:38 +0530

    MirrorPuzzle v3: Demos.elm

---
+++ src/MirrorPuzzleV3/Demos.elm
@@ -0,0 +1,28 @@
+module MirrorPuzzleV3.Demos exposing (main)
+
+import Html exposing (Html)
+import Html.Attributes exposing (class)
+import MirrorPuzzleV3.SimpleGridDemo as SimpleGridDemo
+import MirrorPuzzleV3.TileGridCollageDemo as TileGridCollageDemo
+import MirrorPuzzleV3.TileGridSvgDemo as TileGridDemo
+
+
+
+-- Main
+
+
+main =
+    Html.div [ class "pa2 inline-flex flex-wrap" ]
+        [ Html.div [ class "inline-flex flex-column" ]
+            [ Html.div [ class "tc pa2" ] [ Html.text "collageDemo" ]
+            , Html.div [ class "pa4 lh-0" ] [ TileGridCollageDemo.viewDemo ]
+            ]
+        , Html.div [ class "inline-flex flex-column" ]
+            [ Html.div [ class "tc pa2" ] [ Html.text "TileGrid" ]
+            , TileGridDemo.viewDemo
+            ]
+        , Html.div [ class "inline-flex flex-column" ]
+            [ Html.div [ class "tc pa2" ] [ Html.text "Grid" ]
+            , SimpleGridDemo.viewDemo
+            ]
+        ]
--- src/MirrorPuzzleV3/Main.elm
+++ src/MirrorPuzzleV3/Main.elm
@@ -1,10 +1,6 @@
 module MirrorPuzzleV3.Main exposing (main)
 
-import Html exposing (Html)
-import Html.Attributes exposing (class)
-import MirrorPuzzleV3.SimpleGridDemo as SimpleGridDemo
-import MirrorPuzzleV3.TileGridCollageDemo as TileGridCollageDemo
-import MirrorPuzzleV3.TileGridSvgDemo as TileGridDemo
+import MirrorPuzzleV3.Demos as Demos
 
 
 
@@ -12,17 +8,4 @@ import MirrorPuzzleV3.TileGridSvgDemo as TileGridDemo
 
 
 main =
-    Html.div [ class "pa2 inline-flex flex-wrap" ]
-        [ Html.div [ class "inline-flex flex-column" ]
-            [ Html.div [ class "tc pa2" ] [ Html.text "collageDemo" ]
-            , Html.div [ class "pa4 lh-0" ] [ TileGridCollageDemo.viewDemo ]
-            ]
-        , Html.div [ class "inline-flex flex-column" ]
-            [ Html.div [ class "tc pa2" ] [ Html.text "TileGrid" ]
-            , TileGridDemo.viewDemo
-            ]
-        , Html.div [ class "inline-flex flex-column" ]
-            [ Html.div [ class "tc pa2" ] [ Html.text "Grid" ]
-            , SimpleGridDemo.viewDemo
-            ]
-        ]
+    Demos.main

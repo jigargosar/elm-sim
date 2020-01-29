b49ebc514d7f13f58d8d976b4e928716f5a522ac
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-29 15:07:43 +0530

    DrawingBlocks experiment: Drag

--- src/DrawingBlock/Drag.elm
+++ src/DrawingBlock/Drag.elm
@@ -1,15 +1,7 @@
 module DrawingBlock.Drag exposing (Msg, State, subscriptions)
 
-import Basics.Extra exposing (uncurry)
 import Browser.Events as BE
-import IO
 import Json.Decode as JD
-import Number2 exposing (Float2)
-import PointFree exposing (mapEach)
-import Process
-import Task
-import Time
-import VirtualDom
 
 
 type State
@@ -20,43 +12,25 @@ type State
 
 type Msg
     = OnMouseDown
-    | OnDragStart
     | OnDrag
     | OnClick
     | OnDrop
 
 
-update : Msg -> State -> ( State, Cmd Msg )
+update : Msg -> State -> State
 update message model =
     case message of
         OnMouseDown ->
-            case model of
-                Down ->
-                    ( Down, Process.sleep 1000 |> Task.perform (\_ -> OnDragStart) )
-
-                _ ->
-                    ( model, Cmd.none )
-
-        OnDragStart ->
-            case model of
-                Down ->
-                    ( Drag, Cmd.none )
-
-                _ ->
-                    ( model, Cmd.none )
+            Down
 
         OnDrag ->
-            ( model, Cmd.none )
+            Drag
 
         OnClick ->
-            ( Up, Cmd.none )
+            Up
 
         OnDrop ->
-            ( Up, Cmd.none )
-
-
-tooFar =
-    False
+            Up
 
 
 subscriptions : State -> Sub Msg
@@ -67,13 +41,7 @@ subscriptions state =
 
         Down ->
             [ BE.onMouseUp (JD.succeed OnClick)
-            , BE.onMouseMove
-                (if tooFar then
-                    JD.succeed OnDragStart
-
-                 else
-                    JD.fail "ignoring-move"
-                )
+            , BE.onMouseMove (JD.succeed OnDrag)
             ]
                 |> Sub.batch
 

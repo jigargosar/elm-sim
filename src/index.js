b9671f618e0e310a481b7f4b642dfcde99bffff4
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-30 12:04:31 +0530

    DrawingBlocks experiment: Drag

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const Module = require('./Main.elm')
 require('tachyons')
 require('./styles.css')
+const dat = require('dat.gui')
 
 // console.log(Module)
+const gui = new dat.GUI()
+
+const elmData = { zoom: 1 }
+
+gui.add(elmData, 'zoom').step(0.5).min(0.1).max(50)
 
 Module.Elm.Main.init({
   node: document.getElementById('root'),

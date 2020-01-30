df1afe7faddcdb4d0368e7f97f6f2d133def34bb
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-30 12:09:49 +0530

    DrawingBlocks experiment: Dat.GUI

--- src/index.js
+++ src/index.js
@@ -8,9 +8,19 @@ const gui = new dat.GUI()
 
 const elmData = { zoom: 1 }
 
-gui.add(elmData, 'zoom').step(0.5).min(0.1).max(50)
+gui
+  .add(elmData, 'zoom')
+  .step(0.5)
+  .min(0.1)
+  .max(50)
+  .onChange(function(...args) {
+    console.log('zoomChange', this, args)
+  })
+  .onFinishChange(function(...args) {
+    console.log('zoomFinishChange', this, args)
+  })
 
-Module.Elm.Main.init({
+const app = Module.Elm.Main.init({
   node: document.getElementById('root'),
   flags: {
     now: Date.now(),

3853579059169d0f12144dd04f0af76f3973ba64
Jigar Gosar <jigar.gosar@gmail.com>
2020-01-31 16:38:09 +0530

    DrawingBlocks experiment

--- src/DrawingBlock/Main.elm
+++ src/DrawingBlock/Main.elm
@@ -1,62 +1,20 @@
-port module DrawingBlock.Main exposing (main)
-
--- Browser.Element Scaffold
+module DrawingBlock.Main exposing (main)
 
 import Browser
+import Browser.Dom as BD
 import Browser.Events as BE
-import DrawingBlock.Draggable as Draggable
 import Html as H exposing (Html)
-import IO
-import Json.Decode as JD
-import Number2 as NT exposing (Float2, Int2)
-import Round
-import Svg as S
-import Svg.Attributes as SA
 import Task
-
-
-type alias DatGUIModel =
-    { zoom : Float
-    }
-
-
-port onDatGUIChange : (DatGUIModel -> msg) -> Sub msg
 
 
 
 -- Model
 
 
-type alias Timestamp =
-    Float
-
-
-type alias EventData =
-    ( Timestamp, Float2 )
-
-
 type alias Model =
-    { zoom : Float
-    , pan : Float2
-    , scene : Float2
-    , draggableState : Draggable.State
-    , dragging : Maybe Dragging
+    { width : Float
+    , height : Float
     }
-
-
-type Dragging
-    = Zooming Float2
-    | Panning
-
-
-isDraggingZoom : { a | dragging : Maybe Dragging } -> Bool
-isDraggingZoom model =
-    case model.dragging of
-        Just (Zooming _) ->
-            True
-
-        _ ->
-            False
 
 
 type alias Flags =
@@ -65,13 +23,10 @@ type alias Flags =
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( { zoom = 2.5
-      , pan = ( 0, 0 )
-      , scene = ( 600, 600 )
-      , draggableState = Draggable.intial
-      , dragging = Nothing
+    ( { width = 600
+      , height = 600
       }
-    , IO.getBrowserSize |> Task.perform GotBrowserSize
+    , BD.getViewport |> Task.perform GotViewport
     )
 
 
@@ -80,111 +35,27 @@ init _ =
 
 
 type Msg
-    = UpdateDrag Draggable.State Draggable.Event
-    | TriggerDragZoom Draggable.State IO.MouseEvent
-    | TriggerDragPan Draggable.State IO.MouseEvent
-    | GotBrowserSize Float2
-    | OnKeyDown String
-    | OnDatGUIChange DatGUIModel
+    = GotViewport BD.Viewport
+    | OnBrowserResize Int Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
-        TriggerDragZoom draggableState e ->
-            ( { model | dragging = Just (Zooming ( 0, 0 )), draggableState = draggableState }, Cmd.none )
-
-        TriggerDragPan draggableState _ ->
-            ( { model | dragging = Just Panning, draggableState = draggableState }, Cmd.none )
-
-        UpdateDrag draggableState draggableMsg ->
-            ( { model | draggableState = draggableState } |> handleDragEvents draggableMsg
-            , Cmd.none
-            )
-
-        GotBrowserSize wh ->
-            ( { model | scene = wh }, Cmd.none )
-
-        OnKeyDown key ->
-            ( case key of
-                "1" ->
-                    zoomIn model
-
-                "2" ->
-                    zoomOut model
-
-                _ ->
-                    model
-            , Cmd.none
-            )
-
-        OnDatGUIChange datGUIModel ->
+        GotViewport viewport ->
             let
-                _ =
-                    Debug.log "datGUIModel" datGUIModel
+                ( width, height ) =
+                    ( viewport.scene.width, viewport.scene.height )
             in
-            ( { model | zoom = datGUIModel.zoom }, Cmd.none )
+            ( { model | width = width, height = height }, Cmd.none )
 
-
-handleDragEvents : Draggable.Event -> Model -> Model
-handleDragEvents event model =
-    case ( event, model.dragging ) of
-        ( Draggable.OnDrag { movementXY }, Just (Zooming _) ) ->
-            let
-                ( _, dy ) =
-                    movementXY |> NT.negateSecond
-
-                zoomStep =
-                    dy * 0.01 * model.zoom
-
-                zoom =
-                    model.zoom + zoomStep
-            in
-            { model
-                | zoom = zoom
-
-                --, pan = model.pan |> NT.scale model.zoom |> NT.add zoomOrigin |> NT.scale (1 / zoom)
-            }
-
-        ( Draggable.OnDrag { movementXY }, Just Panning ) ->
-            let
-                panStep =
-                    movementXY |> NT.negateSecond |> NT.scale (1 / model.zoom)
-            in
-            { model | pan = NT.add model.pan panStep }
-
-        ( Draggable.OnEnd _ _, _ ) ->
-            { model | dragging = Nothing }
-
-        _ ->
-            model
-
-
-setZoomUpdatePan zoom model =
-    { model
-        | zoom = zoom
-        , pan = model.pan |> NT.scale model.zoom |> NT.scale (1 / zoom)
-    }
-
-
-zoomIn model =
-    setZoomUpdatePan (model.zoom |> (\s -> clamp 0.05 50 (s + s * 0.1))) model
-
-
-zoomOut model =
-    setZoomUpdatePan (model.zoom |> (\s -> clamp 0.05 50 (s + s * 0.1))) model
-
-
-keyDecoder =
-    JD.field "key" JD.string
+        OnBrowserResize width height ->
+            ( { model | width = toFloat width, height = toFloat height }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
-    [ IO.onBrowserResize GotBrowserSize
-    , JD.map OnKeyDown keyDecoder |> BE.onKeyDown
-    , Draggable.subscriptions UpdateDrag model.draggableState
-    , onDatGUIChange OnDatGUIChange
+subscriptions _ =
+    [ BE.onResize OnBrowserResize
     ]
         |> Sub.batch
 
@@ -194,64 +65,8 @@ subscriptions model =
 
 
 view : Model -> Html Msg
-view model =
-    H.div []
-        [ IO.styleNode globalStyles
-        , IO.canvas model.scene
-            [ Draggable.onMouseDown TriggerDragPan
-            ]
-            [ IO.group [ IO.transform [ IO.scale model.zoom, IO.shift model.pan ] ]
-                (drawing model)
-            ]
-        ]
-
-
-globalStyles =
-    """
-    .ns-resize:hover {
-        cursor: ns-resize;
-    }
-
-    .fill-green, .hover-fill-green:hover {
-        fill: green;
-    }
-    """
-
-
-drawing : Model -> List (S.Svg Msg)
-drawing model =
-    [ viewZoomData (isDraggingZoom model) model.zoom
-    ]
-
-
-viewZoomData : Bool -> Float -> S.Svg Msg
-viewZoomData forceHover zoom =
-    [ IO.tspan "Zoom = " [ SA.fill "black" ]
-    , IO.tspan (Round.round 2 zoom)
-        []
-    , S.style []
-        (if forceHover then
-            [ S.text
-                """
-                    body {
-                        cursor: ns-resize;
-                    }
-                """
-            ]
-
-         else
-            []
-        )
-    ]
-        |> IO.textGroup
-            [ Draggable.onMouseDown TriggerDragZoom
-            , SA.class "ns-resize"
-            , if forceHover then
-                SA.class "fill-green"
-
-              else
-                SA.class "hover-fill-green"
-            ]
+view _ =
+    H.div [] []
 
 
 empty : Html msg

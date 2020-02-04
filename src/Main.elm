62a2ae4ed40dec4489d4e97061b528c31f8b885d
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-04 15:08:16 +0530

    Shapes API

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,7 @@
 module Main exposing (main)
 
-import Html.Attributes as HA
-import String exposing (fromFloat)
-import Svg as S exposing (svg, text, text_)
-import Svg.Attributes as SA exposing (dominantBaseline, fill, textAnchor)
-import TypedSvg.Attributes as TA exposing (viewBox)
+import MainSvgCanvas as MC
+import String
 
 
 type alias NodeAttributes =
@@ -38,10 +35,10 @@ drawNode node =
                 { x, y, width, height } =
                     shape
             in
-            rect shape.fill width height [ transform [ shift ( x, y ) ] ]
+            MC.rect shape.fill width height [ MC.transform [ MC.shift ( x, y ) ] ]
 
         Layer nodeAttributes nodes ->
-            group [] (List.map drawNode nodes)
+            MC.group [] (List.map drawNode nodes)
 
 
 main =
@@ -49,107 +46,8 @@ main =
         l1 =
             newLayer [ newRect 300 400 ]
     in
-    canvas ( 600, 600 )
+    MC.canvas ( 600, 600 )
         []
-        [ rect "blue" 100 100 []
+        [ MC.rect "blue" 100 100 []
         , drawNode l1
         ]
-
-
-
--- SVG CANVAS LIB
-
-
-canvas ( w, h ) attrs =
-    let
-        ( x, y ) =
-            ( -w / 2, -h / 2 )
-    in
-    svg
-        (viewBox x y w h
-            :: SA.shapeRendering "optimizeSpeed"
-            :: HA.style "position" "fixed"
-            :: HA.style "top" "0"
-            :: HA.style "left" "0"
-            :: HA.style "width" "100%"
-            :: HA.style "height" "100%"
-            :: attrs
-        )
-
-
-strokeWidth =
-    fromFloat >> SA.strokeWidth
-
-
-group =
-    S.g
-
-
-words color string attrs =
-    text_
-        (textAnchor "middle"
-            :: dominantBaseline "central"
-            :: fill color
-            :: attrs
-        )
-        [ text string ]
-
-
-square c w =
-    rect c w w
-
-
-rect color width height attrs =
-    let
-        ( x, y ) =
-            ( width / 2, height / 2 )
-    in
-    S.polygon
-        (TA.points [ ( -x, -y ), ( x, -y ), ( x, y ), ( -x, y ) ]
-            :: fill color
-            :: attrs
-        )
-        []
-
-
-type alias Transform =
-    { x : Float
-    , y : Float
-    , s : Float
-    , deg : Float
-    }
-
-
-identityTransform =
-    Transform 0 0 1 0
-
-
-scale n t =
-    { t | s = n }
-
-
-shift ( dx, dy ) t =
-    { t | x = t.x + dx, y = t.y + dy }
-
-
-transform =
-    List.foldl (<|) identityTransform
-        >> transformToString
-        >> SA.transform
-
-
-transformToString { x, y, s, deg } =
-    let
-        t name args =
-            String.concat
-                [ name
-                , "("
-                , String.join " " (List.map String.fromFloat args)
-                , ")"
-                ]
-    in
-    t "translate" [ x, y ]
-        ++ " "
-        ++ t "scale" [ s ]
-        ++ " "
-        ++ t "rotate" [ deg ]

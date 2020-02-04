f9f5f93c383c960241f670a46f96e2de5011879b
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-04 15:12:59 +0530

    Shapes API

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,76 @@
 module Main exposing (main)
 
+-- Browser.Element Scaffold
+
+import Browser
+import Html exposing (Html)
 import MainStage exposing (group, rect, stage)
 
 
-main =
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch []
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view _ =
     stage ( 600, 600 )
         [ group [ rect 300 400 ]
         ]
+
+
+empty : Html msg
+empty =
+    Html.text ""
+
+
+
+-- Main
+
+
+main : Program Flags Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }

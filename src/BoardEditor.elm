fd9d69c0854db1b531eafc46591735df94a71512
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-09 20:30:04 +0530

    Dialog

--- src/BoardEditor.elm
+++ src/BoardEditor.elm
@@ -5,18 +5,15 @@ port module BoardEditor exposing (main)
 import Browser
 import Browser.Dom
 import CD
-import Dict exposing (Dict)
-import Element as E exposing (none, padding, paddingXY, spacing, text)
+import Element as E exposing (none, padding, paddingXY, spacing)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Events as Events
 import Element.Font as Font
-import Element.Input as Input
 import Html exposing (Html)
 import Inst
 import Prog exposing (LayerName)
 import Program.Builder as B
-import Program.Zipper as Z
 import String exposing (fromInt)
 import Task
 
@@ -35,41 +32,9 @@ type alias Int2 =
     ( Int, Int )
 
 
-type alias GridDict a =
-    Dict Int2 a
-
-
-type alias DirectionGrid =
-    GridDict Direction
-
-
-type Direction
-    = Up
-    | Down
-    | Left
-    | Right
-
-
-type DirectionInstruction
-    = DirectionChange Direction
-    | DirectionNoChange
-
-
-type ReactorInstruction
-    = Start
-    | In
-    | Out
-    | Grab
-    | Drop
-    | NOP
-
-
 type alias Model =
     { width : Int
     , height : Int
-    , dirGrid : DirectionGrid
-    , riGrid : GridDict ReactorInstruction
-    , edit : Edit
     , scrollbarSize : ( Int, Int )
     , dialog : Dialog
     }
@@ -78,12 +43,6 @@ type alias Model =
 type Dialog
     = ArrowDialog LayerName Int Int
     | NoDialog
-
-
-type Edit
-    = NoEdit
-    | EditDI Int Int
-    | EditRI Int Int
 
 
 type alias Flags =
@@ -97,27 +56,9 @@ init flags =
     let
         _ =
             Debug.log "flags" flags
-
-        dirGrid =
-            Dict.fromList
-                [ ( ( 0, 0 ), Down ) ]
-
-        riGrid =
-            Dict.fromList
-                [ ( ( 0, 0 ), Start ) ]
-
-        _ =
-            Z.init 10 8
-                |> Z.switchToBlue
-                |> Z.switchToRed
-                |> Z.go 4 1
-                |> Z.set Inst.Start CD.left
     in
     ( { width = 10
       , height = 8
-      , dirGrid = dirGrid
-      , riGrid = riGrid
-      , edit = NoEdit
       , scrollbarSize = flags.scrollbarSize
       , dialog = NoDialog
       }
@@ -134,10 +75,6 @@ init flags =
 
 type Msg
     = NoOp
-    | StartEditDI Int Int
-    | StartEditRI Int Int
-    | DISelected DirectionInstruction
-    | RISelected ReactorInstruction
     | ShowArrowDialog Prog.LayerName Int Int
     | DialogBackgroundClicked
     | GotScrollbarSize Int2
@@ -148,38 +85,6 @@ update message model =
     case message of
         NoOp ->
             ( model, Cmd.none )
-
-        StartEditDI x y ->
-            ( { model | edit = EditDI x y }, Cmd.none )
-
-        StartEditRI x y ->
-            ( { model | edit = EditRI x y }, Cmd.none )
-
-        DISelected di ->
-            case model.edit of
-                EditDI x y ->
-                    ( { model
-                        | dirGrid = setDirectionInstruction x y di model.dirGrid
-                        , edit = NoEdit
-                      }
-                    , Cmd.none
-                    )
-
-                _ ->
-                    ( model, Cmd.none )
-
-        RISelected ri ->
-            case model.edit of
-                EditRI x y ->
-                    ( { model
-                        | riGrid = setRI x y ri model.riGrid
-                        , edit = NoEdit
-                      }
-                    , Cmd.none
-                    )
-
-                _ ->
-                    ( model, Cmd.none )
 
         GotScrollbarSize s ->
             ( { model | scrollbarSize = s }, Cmd.none )
@@ -202,16 +107,6 @@ subscriptions _ =
 -- View
 
 
-directionInstructionAt : Int -> Int -> DirectionGrid -> DirectionInstruction
-directionInstructionAt x y =
-    Dict.get ( x, y ) >> unwrap DirectionNoChange DirectionChange
-
-
-riAt : Int -> Int -> RIGrid -> ReactorInstruction
-riAt x y =
-    Dict.get ( x, y ) >> Maybe.withDefault NOP
-
-
 unwrap default func maybe =
     case maybe of
         Just a ->
@@ -219,25 +114,6 @@ unwrap default func maybe =
 
         Nothing ->
             default
-
-
-setDirectionInstruction : Int -> Int -> DirectionInstruction -> DirectionGrid -> DirectionGrid
-setDirectionInstruction x y di =
-    case di of
-        DirectionNoChange ->
-            Dict.remove ( x, y )
-
-        DirectionChange direction ->
-            Dict.insert ( x, y ) direction
-
-
-type alias RIGrid =
-    GridDict ReactorInstruction
-
-
-setRI : Int -> Int -> ReactorInstruction -> RIGrid -> RIGrid
-setRI x y =
-    Dict.insert ( x, y )
 
 
 black =
@@ -260,54 +136,16 @@ lightGray =
     E.rgb255 200 200 200
 
 
-isEditingDIAt : Int -> Int -> Edit -> Bool
-isEditingDIAt x y edit =
-    EditDI x y == edit
-
-
-isEditingRIAt : Int -> Int -> Edit -> Bool
-isEditingRIAt x y edit =
-    EditRI x y == edit
-
-
 view : Model -> Html Msg
 view model =
-    let
-        viewCellHelp x y =
-            let
-                di =
-                    directionInstructionAt x y model.dirGrid
-
-                showDIEditor =
-                    isEditingDIAt x y model.edit
-
-                ri =
-                    riAt x y model.riGrid
-
-                showRIEditor =
-                    isEditingRIAt x y model.edit
-            in
-            viewCell x y di showDIEditor ri showRIEditor
-    in
     E.layout
-        [ E.inFront
-            (viewDialog model)
+        [ E.inFront (viewDialog model)
         , E.height E.fill
         ]
         (E.column
             [ E.width E.fill
             ]
-            [ viewProg
-            , renderGrid
-                [ Border.width 1
-                , Border.color lightGray
-                , E.centerX
-                , E.width E.shrink
-                ]
-                model.width
-                model.height
-                viewCellHelp
-            ]
+            [ viewProg ]
         )
 
 
@@ -459,112 +297,6 @@ viewProg =
         10
         8
         viewCellHelp
-
-
-viewCell : Int -> Int -> DirectionInstruction -> Bool -> ReactorInstruction -> Bool -> E.Element Msg
-viewCell x y di showDIEditor ri showRIEditor =
-    let
-        viewDI =
-            let
-                ( diString, diColor ) =
-                    case di of
-                        DirectionChange d ->
-                            ( Debug.toString d, black )
-
-                        DirectionNoChange ->
-                            ( "noc", black )
-            in
-            E.el
-                [ Font.color diColor
-                , E.pointer
-                , E.padding 5
-                , if showDIEditor then
-                    E.inFront (viewDIEditor di)
-
-                  else
-                    Events.onClick (StartEditDI x y)
-                ]
-                (E.text diString)
-
-        viewRI =
-            let
-                ( riString, riColor ) =
-                    case ri of
-                        NOP ->
-                            ( "nop", black )
-
-                        _ ->
-                            ( Debug.toString ri, black )
-            in
-            E.el
-                [ Font.color riColor
-                , E.pointer
-                , E.padding 5
-                , if showRIEditor then
-                    E.inFront (viewRIEditor ri)
-
-                  else
-                    Events.onClick (StartEditRI x y)
-                ]
-                (E.text riString)
-
-        viewIdx =
-            E.el
-                [ E.padding 5
-                , Font.size 14
-                , Font.color lightGray
-                ]
-                (E.text (fromInt x ++ "," ++ fromInt y))
-    in
-    E.column
-        [ Border.width 1
-        , Border.color lightGray
-        , E.padding 5
-        , E.width (E.minimum 80 E.fill)
-        ]
-        [ viewIdx
-        , viewDI
-        , viewRI
-        , E.text "HH"
-        ]
-
-
-viewDIEditor di =
-    Input.radio
-        [ padding 10
-        , spacing 20
-        , Background.color lightGray
-        , Font.color black
-        ]
-        { onChange = DISelected
-        , selected = Just di
-        , label = Input.labelHidden ""
-        , options =
-            [ Input.option (DirectionChange Up) (text "Up")
-            , Input.option (DirectionChange Down) (text "Down")
-            , Input.option (DirectionChange Left) (text "Left")
-            , Input.option (DirectionChange Right) (text "Right")
-            , Input.option DirectionNoChange (text "noc")
-            ]
-        }
-
-
-viewRIEditor : ReactorInstruction -> E.Element Msg
-viewRIEditor ri =
-    Input.radio
-        [ padding 10
-        , spacing 20
-        , Background.color lightGray
-        , Font.color black
-        ]
-        { onChange = RISelected
-        , selected = Just ri
-        , label = Input.labelHidden ""
-        , options =
-            [ Input.option Start (text "Start")
-            , Input.option NOP (text "nop")
-            ]
-        }
 
 
 renderGrid attrs width height viewFunc =

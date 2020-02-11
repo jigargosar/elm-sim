e428d99401cff74ac413f40cb4d78be00f9704b9
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-11 19:25:59 +0530

    Tetras Model

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,30 @@
 module Main exposing (main)
 
-import Html
+import Html exposing (div, text)
+import Html.Attributes exposing (class)
+
+
+type Shape
+    = Square
+    | Line
+    | Tee
+    | S
 
 
 main =
-    Html.text "RESET"
+    let
+        viewSquare =
+            """
+            0000
+            0110
+            0110
+            0000
+            """
+                |> String.trim
+                |> String.split "\n"
+                |> List.map String.trim
+                |> String.join "\n"
+    in
+    div [ class "mono-board" ]
+        [ text viewSquare
+        ]

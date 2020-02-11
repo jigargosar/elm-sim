eb4e6751f49f8e9224c216ba9dd80db0bc3bec3d
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-11 15:07:11 +0530

    Archiving to src1

--- src/index.js
+++ src/index.js
@@ -1,39 +1,12 @@
-// const Module = require('./Main.elm')
-// const Module = require('./BoardEditor.elm')
-const Module = require('./TodoistInterface.elm')
-// const Module = require('./ReactorSimulation.elm')
-// require('tachyons')
+const Module = require('./Main.elm')
 require('./styles.css')
 
 {
-  const app = initElmApp()
-  // app.ports.getScrollbarSize.subscribe(function() {
-  //   app.ports.gotScrollbarSize.send([
-  //     window.innerWidth - document.body.clientWidth,
-  //     window.innerHeight - document.body.clientHeight,
-  //   ])
-  // })
-  window.addEventListener('resize', function() {
-    // app.ports.gotViewSize.send([window.innerWidth, window.innerHeight])
-  })
-
-  app.ports.focusIdNextTick.subscribe(function(hid) {
-    setTimeout(function() {
-      const el = document.getElementById(hid)
-      if (el) {
-        el.focus()
-      } else {
-        console.warn('Focus Failed: NotFound ' + hid)
-      }
-    }, 0)
-  })
+  initElmApp()
 }
 
 function initElmApp() {
-  // const app = Module.Elm.Main.init({
-  const app = Module.Elm.TodoistInterface.init({
-    // const app = Module.Elm.BoardEditor.init({
-    // const app = Module.Elm.ReactorSimulation.init({
+  const app = Module.Elm.Main.init({
     node: document.getElementById('root'),
     flags: {
       now: Date.now(),

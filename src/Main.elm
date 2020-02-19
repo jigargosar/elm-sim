58bd304f8bc69cd3ea96984688119e81cd883c8d
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-19 15:27:28 +0530

    Prototyping UI

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,9 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (style)
+import String exposing (fromInt)
 
 
 
@@ -53,21 +55,47 @@ type alias OutlineView =
 
 
 type NodeId
-    = NodeId
+    = NodeId String
 
 
 type NodeData
-    = NodeData
+    = NodeData String
 
 
 type Node
-    = LeafNode NodeId NodeData
-    | Node NodeId NodeData (List Node)
+    = Node NodeId NodeData (List Node)
+
+
+ov : OutlineView
+ov =
+    { list =
+        [ Node (NodeId "10") (NodeData "First Node") []
+        , Node (NodeId "20") (NodeData "Second Node") []
+        , Node (NodeId "30")
+            (NodeData "Third Node ")
+            [ Node (NodeId "30-10") (NodeData "C1 Node ") []
+            , Node (NodeId "30-20") (NodeData "C2 Node ") []
+            , Node (NodeId "30-30") (NodeData "C3 Node ") []
+            ]
+        ]
+    }
 
 
 view : Model -> Html Msg
 view _ =
-    empty
+    div []
+        [ div [] [ text "Outline View" ]
+        , div [] (List.concatMap (viewNode 0) ov.list)
+        ]
+
+
+viewNode level (Node (NodeId id) (NodeData data) children) =
+    div
+        [ style "padding-left" <| fromInt (level * 30) ++ "px"
+        ]
+        [ text <| "id: " ++ id ++ ". data: " ++ data
+        ]
+        :: List.concatMap (viewNode (level + 1)) children
 
 
 empty : Html msg

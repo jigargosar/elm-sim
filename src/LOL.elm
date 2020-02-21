a807e13f7294196476ae86014319ebd8ec1ff720
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-21 15:59:36 +0530

    Modeling LOL: List Of Lists

--- src/LOL.elm
+++ src/LOL.elm
@@ -55,8 +55,10 @@ viewSample =
                 [ ni "x: Show Group title with items"
                 , ni "x: should we show group list separately? and on nav show children? Or show entire tree?"
                 , ni "x: For now it seems easier to show everything"
-                , ni "now showing focus is complicated, since we are showing both, group &  it's items"
-                , ni "perhaps not, we can have group-pivot, and for that group, item pivot. humm.."
+                , ni "x: now showing focus is complicated, since we are showing both, group &  it's items"
+                , ni "x: perhaps not, we can have group-pivot, and for that group, item pivot. humm.."
+                , ni "x: I think we should keep things simple. one view for master list, then sub view for its list items"
+                , ni "how do we model group list view, group items view?"
                 ]
             , ng "Projects"
                 [ ni "LOL Demo"
